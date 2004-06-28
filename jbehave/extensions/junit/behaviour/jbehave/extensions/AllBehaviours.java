0c143cbd89036dc9b03d4622c18695e3e3961c57
dan <dan@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-06-28 16:35:04 +0000

    Initial checkin

---
+++ jbehave/extensions/junit/behaviour/jbehave/extensions/AllBehaviours.java
@@ -0,0 +1,26 @@
+/*
+ * Created on 27-Jun-2004
+ * 
+ * (c) 2003-2004 ThoughtWorks Ltd
+ *
+ * See license.txt for license details
+ */
+package jbehave.extensions;
+
+import jbehave.extensions.jmock.listener.JMockListenerBehaviour;
+import jbehave.extensions.junit.adapter.JUnitAdapterBehaviours;
+import jbehave.framework.Aggregate;
+
+/**
+ * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
+ */
+public class AllBehaviours implements Aggregate {
+
+	public Class[] getBehaviourClasses() {
+        return new Class[] {
+            JUnitAdapterBehaviours.class,
+            JMockListenerBehaviour.class
+        };
+	}
+
+}
---
+++ jbehave/extensions/junit/behaviour/jbehave/extensions/junit/AllBehavioursSuite.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 29-Dec-2003
+ * 
+ * (c) 2003-2004 ThoughtWorks
+ * 
+ * See license.txt for licence details
+ */
+package jbehave.extensions.junit;
+import jbehave.AllBehaviours;
+import jbehave.extensions.junit.adapter.JUnitAdapter;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * So we can run all the behaviours inside the junit runner
+ * 
+ * @author <a href="mailto:dan@jbehave.org">Dan North</a>
+ */
+public class AllBehavioursSuite {
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        Class[] behaviourClasses = new AllBehaviours().getBehaviourClasses();
+        for (int i = 0; i < behaviourClasses.length; i++) {
+            suite.addTest(wrap(behaviourClasses[i]));
+        }
+        return suite;
+    }
+
+    private static Test wrap(Class behaviourClass) {
+        return new JUnitAdapter(behaviourClass);
+    }
+}
