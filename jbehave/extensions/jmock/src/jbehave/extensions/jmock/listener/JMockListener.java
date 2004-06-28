affe00fd8d46f0cf118e39994744929ba7eb9f93
dan <dan@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-06-28 20:12:22 +0000

    Tidying up

--- jbehave/extensions/jmock/src/jbehave/extensions/jmock/listener/JMockListener.java
+++ jbehave/extensions/jmock/src/jbehave/extensions/jmock/listener/JMockListener.java
@@ -12,28 +12,22 @@ import java.lang.reflect.Field;
 import jbehave.framework.BehaviourResult;
 import jbehave.runner.listener.ListenerSupport;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.jmock.Mock;
 
 /**
  * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
  */
 public class JMockListener extends ListenerSupport {
-    private static final Log log = LogFactory.getLog(JMockListener.class);
 
 	public void behaviourEnded(BehaviourResult behaviourResult) {
-        log.trace("behaviourEnded");
         Object executedInstance = behaviourResult.getExecutedInstance();
         
         // iterate looking for fields of type Mock
         Field[] fields = executedInstance.getClass().getDeclaredFields();
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
-            log.trace("Field = " + field + ", " + " type = " + field.getType(), null);
             if (Mock.class.equals(field.getType())) {
                 try {
-                    log.trace("Testing field " + field);
                     if (!field.isAccessible()) {
                         field.setAccessible(true);
                     }
