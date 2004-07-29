c9a2a51568987e383174603d4380e95ea09e62c1
damian <damian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-07-29 09:57:13 +0000

    update junit adapter

--- jbehave/extensions/junit/src/jbehave/extensions/junit/adapter/JUnitAdapter.java
+++ jbehave/extensions/junit/src/jbehave/extensions/junit/adapter/JUnitAdapter.java
@@ -9,79 +9,94 @@ package jbehave.extensions.junit.adapter;
 
 import jbehave.framework.CriteriaVerifier;
 import jbehave.framework.CriteriaVerification;
-import jbehave.framework.CriteriaExtractor;
-import jbehave.framework.VerificationException;
-import jbehave.verify.Verifier;
-import jbehave.verify.listener.ListenerSupport;
+import jbehave.framework.SpecVerifier;
+import jbehave.framework.exception.VerificationException;
+import jbehave.listeners.NullListener;
 import junit.framework.Test;
 import junit.framework.TestResult;
+import junit.framework.TestCase;
+
+import java.lang.reflect.Method;
 
 /**
  * Allows behaviours to be run in any JUnit runner
- * 
+ *
  * @author <a href="mailto:dan@jbehave.org">Dan North</a>
  * @author <a href="mailto:joe@jbehave.org">Joe Walnes</a>
+ * @author <a href="mailto:damian.guy@thoughtworks.com">Damian Guy</a>
  */
-public class JUnitAdapter implements Test {
+public class JUnitAdapter extends TestCase {
 
-    private final class JUnitListener extends ListenerSupport {
-        private final TestResult result;
-        private final Test adapter;
-        private JUnitListener(TestResult result, Test adapter) {
-            this.result = result;
-            this.adapter = adapter;
-        }
-        public void verificationStarted(Verifier runner) {
-        }
-        public void specVerificationStarted(Class behaviourClass) {
-            setBehaviourClassName(behaviourClass.getName());
-            result.startTest(adapter);
-        }
-        public void beforeCriteriaVerificationStarts(CriteriaVerifier behaviour) {
-        }
-        public void afterCriteriaVerificationEnds(CriteriaVerification behaviourResult) {
-            if (behaviourResult.failed()) {
-                VerificationException e = (VerificationException)behaviourResult.getTargetException();
-                result.addError(adapter, e);
-            }
-            else if (behaviourResult.threwException()) {
-                result.addError(adapter, behaviourResult.getTargetException());
-            }
-            result.endTest(adapter);                 
-        }
-    }
+	private final class JUnitListener extends NullListener {
+		private final TestResult result;
+		private final Test adapter;
+        private Class currentSpec;
 
-    private final Class behaviours;
-    private String currentBehaviourName;
-    
-    public JUnitAdapter(Class behaviours) {
-        this(behaviours, 1);
-    }
-    
-    public JUnitAdapter(Class behaviours, int depth) {
-        this.behaviours = behaviours;
-        currentBehaviourName = behaviours.getName();
-    }
+		private JUnitListener(TestResult result, Test adapter) {
+			this.result = result;
+			this.adapter = adapter;
+		}
 
-    public int countTestCases() {
-        return new CriteriaExtractor(behaviours).createCriteriaVerifiers().size();
-    }
+		public void specVerificationStarting(Class spec) {
+			currentSpec = spec;
+		}
 
-    private void setBehaviourClassName(String behaviourClassName) {
-        currentBehaviourName = behaviourClassName;
-    }
+		public void criteriaVerificationStarting(CriteriaVerifier verifier, Object spec) {
+			setCurrentCriteria(currentSpec.getName());
 
-    public void run(final TestResult result) {        
-        Verifier currentRunner = new Verifier();
-        currentRunner.addSpec(behaviours);
-        
-        currentRunner.registerListener(
-                new JUnitListener(result, this)
-        );
-        currentRunner.verifyCriteria();
-    }
+			result.startTest(adapter);
+		}
 
-    public String toString() {
-        return currentBehaviourName;
-    }
+		public CriteriaVerification criteriaVerificationEnding(CriteriaVerification verification, Object specInstance) {
+			if (verification.failed()) {
+				VerificationException e = (VerificationException) verification.getTargetException();
+				result.addError(adapter, e);
+			} else if (verification.threwException()) {
+				result.addError(adapter, verification.getTargetException());
+			}
+			result.endTest(adapter);
+			return verification;
+		}
+
+	}
+
+	private final Class behaviours;
+	private String currentBehaviourName;
+
+	public JUnitAdapter(Class behaviours) {
+		this(behaviours, 1);
+	}
+
+	public JUnitAdapter(Class behaviours, int depth) {
+		this.behaviours = behaviours;
+		currentBehaviourName = behaviours.getName();
+	}
+
+	public String getName() {
+		return currentBehaviourName;
+	}
+
+	public int countTestCases() {
+		Method [] methods = behaviours.getMethods();
+		int count = 0;
+		for (int i = 0; i < methods.length; i++) {
+			Method method = methods[i];
+			if (method.getName().startsWith("should")) count ++;
+		}
+		return count;
+	}
+
+	private void setCurrentCriteria(String behaviourClassName) {
+		currentBehaviourName = behaviourClassName;
+	}
+
+	public void run(final TestResult result) {
+		SpecVerifier currentRunner = new SpecVerifier(behaviours);
+
+		currentRunner.verifySpec(new JUnitListener(result, this));
+	}
+
+	public String toString() {
+		return currentBehaviourName;
+	}
 }
