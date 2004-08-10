657dcfc26dac4ffb10a2f290a8e84958449eecab
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-08-10 17:19:24 +0000

    Updated source snapshot target in uberbuild

--- trunk/jbehave/extensions/jmock/src/jbehave/extensions/jmock/UsingJMock.java
+++ trunk/jbehave/extensions/jmock/src/jbehave/extensions/jmock/UsingJMock.java
@@ -1,6 +1,6 @@
 /*
  * Created on 09-Aug-2004
- * 
+ *
  * (c) 2003-2004 ThoughtWorks Ltd
  *
  * See license.txt for license details
@@ -22,21 +22,21 @@ import org.jmock.core.DynamicMock;
 public interface UsingJMock extends JMockMixins {
     /**
      * Convenience methods for managing mocks
-     * 
+     *
      * The pixies will ensure that
      * the {@link Mock} gets verified when the Responsibility method ends.
      */
     class Mocks {
         private static final Set mocks = new HashSet();
-        
+
         public static void clear() {
             mocks.clear();
         }
-        
+
         private static void add(Mock mock) {
             mocks.add(mock);
         }
-        
+
         public static void verify() {
             for (Iterator i = mocks.iterator(); i.hasNext();) {
                 Mock mock = null;
@@ -54,31 +54,14 @@ public interface UsingJMock extends JMockMixins {
 
     /**
      * Interceptor class for creating instances of {@link org.jmock.Mock}.
-     * 
+     *
      * On construction, capture a stack trace of the point of construction, and
      * add the new instance to a {@link Set} of <tt>Mock</tt> instances to be
      * verified later.
      */
     class Mock extends org.jmock.Mock {
-        private Exception exceptionFromCreationPoint;
+        private final Exception exceptionFromCreationPoint;
 
-        /**
-         * Set up a fack stack trace pointing back to the point where the
-         * mock was constructed, rather than the obscure part of the framework
-         * that actually does the mocking.
-         */
-        public void fakeExceptionStackTrace(Exception exceptionToFake) {
-            try {
-                StackTraceElement[] creationStackTrace = exceptionFromCreationPoint.getStackTrace();
-                StackTraceElement[] fakedStackTrace =
-                    new StackTraceElement[creationStackTrace.length - 1];
-                System.arraycopy(creationStackTrace, 1, fakedStackTrace, 0, fakedStackTrace.length);
-                exceptionToFake.setStackTrace(fakedStackTrace);
-            } catch (NoSuchMethodError e) {
-                // shame - not running in a 1.4 VM
-            }
-        }
-        
         public Mock(Class mockedType) {
             super(mockedType);
             exceptionFromCreationPoint = new Exception();
@@ -95,6 +78,24 @@ public interface UsingJMock extends JMockMixins {
             super(coreMock);
             exceptionFromCreationPoint = new Exception();
             Mocks.add(this);
+        }
+
+        /**
+         * Set up a fake stack trace pointing back to the point where the
+         * mock was constructed, rather than the obscure part of the framework
+         * that actually does the mocking.
+         */
+        public void fakeExceptionStackTrace(Exception exceptionToFake) {
+            try {
+                StackTraceElement[] creationStackTrace = exceptionFromCreationPoint.getStackTrace();
+                StackTraceElement[] fakedStackTrace =
+                    new StackTraceElement[creationStackTrace.length - 1];
+                System.arraycopy(creationStackTrace, 1, fakedStackTrace, 0, fakedStackTrace.length);
+                exceptionToFake.setStackTrace(fakedStackTrace);
+//                exceptionToFake.printStackTrace();
+            } catch (NoSuchMethodError e) {
+                // shame - not running in a 1.4 VM
+            }
         }
     }
 }
