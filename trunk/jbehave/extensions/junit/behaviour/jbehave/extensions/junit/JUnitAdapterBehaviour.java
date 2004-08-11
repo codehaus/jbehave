0687f2a67fbb81560f9019704024c409aacf001d
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-08-11 12:26:55 +0000

    Added junit. Changed build.

---
+++ trunk/jbehave/extensions/junit/behaviour/jbehave/extensions/junit/JUnitAdapterBehaviour.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 11-Aug-2004
+ * 
+ * (c) 2003-2004 ThoughtWorks Ltd
+ *
+ * See license.txt for license details
+ */
+package jbehave.extensions.junit;
+
+/**
+ * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
+ */
+public class JUnitAdapterBehaviour {
+    public static class SomeBehaviour {
+        public void shouldDoSomething() throws Exception {
+            
+        }
+    }
+    
+    public void shouldRunResponsibilityMethodAsTest() throws Exception {
+        // setup
+//        JUnitAdapter adapter = new JUnitAdapter();
+//        adapter.setBehaviourClassName();
+        
+        // execute
+        
+        
+        // verify
+    }
+}
