d49b5ae9b291ac750281367ccf35cdec513ada26
damian <damian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-09-10 10:11:52 +0000

    add NotifyingResponsibilityVerifierBehaviour

---
+++ trunk/jbehave/core/behaviour/com/thoughtworks/jbehave/core/responsibility/NotifyingResponsibilityVerifierBehaviour.java
@@ -0,0 +1,41 @@
+package com.thoughtworks.jbehave.core.responsibility;
+
+import java.lang.reflect.Method;
+
+import com.thoughtworks.jbehave.core.listeners.ListenerSupport;
+
+/**
+ * @author <a href="mailto:dguy@thoughtworks.com">Damian Guy</a>
+ *
+ */
+public class NotifyingResponsibilityVerifierBehaviour {
+    
+    public static class StubListener extends ListenerSupport {
+        
+        private Result result;
+
+        public StubListener(Result result) {
+            this.result = result;
+        }
+        
+        public Result responsibilityVerificationEnding(Result result,
+                Object behaviourClassInstance) {
+            return this.result;
+           
+        }
+}
+    
+    public void shouldReturnResultFromListener() throws Exception {
+        // setup
+        NotifyingResponsibilityVerifier verifier = new NotifyingResponsibilityVerifier();
+        Result result = new Result("fake", "fake", null);
+        Method method = getClass().getMethods()[0];
+        
+        // execute
+        Result returned = verifier.verifyResponsibility(new StubListener(result), method, null);
+    
+        // verify
+        Verify.that("should return result from listener", result == returned);
+    }
+
+}
