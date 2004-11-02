6f09444985dc5ac37bff3945662863c0c26bdef8
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-02 06:51:20 +0000

    Factored out some interfaces for story runner and forgot to check them in.

---
+++ trunk/jbehave/extensions/story/src/com/thoughtworks/jbehave/extensions/story/domain/Event.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 31-Oct-2004
+ * 
+ * (c) 2003-2004 ThoughtWorks Ltd
+ *
+ * See license.txt for license details
+ */
+package com.thoughtworks.jbehave.extensions.story.domain;
+
+import com.thoughtworks.jbehave.core.Visitable;
+import com.thoughtworks.jbehave.minimock.UsingMocks;
+
+/**
+ * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
+ */
+public interface Event extends Visitable, UsingMocks {
+    void occurIn(Environment environment) throws Exception;
+}
