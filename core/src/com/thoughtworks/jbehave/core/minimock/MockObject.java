06ff64a06178979f7de6972388c773f8c1e1b72c
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-14 22:14:59 +0000

    Forgot to add ScenarioResult. Added boolean Constraints to MiniMockSugar - and(a,b), or(a,b), not(a).

--- core/src/com/thoughtworks/jbehave/core/minimock/MockObject.java
+++ core/src/com/thoughtworks/jbehave/core/minimock/MockObject.java
@@ -12,7 +12,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
