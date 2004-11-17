01b7707f4a412528fb6d4ff2ac4a37b77ec1f6fe
damian <damian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-17 12:36:20 +0000

    Adding CurrencyConverter example

---
+++ trunk/examples/currency/src/org/xpday/currency/SterlingCurrencyConverter.java
@@ -0,0 +1,36 @@
+/*
+ * Created on 15-Nov-2004
+ *
+ * (c) Damian Guy
+ *
+ * See license.txt for licence details
+ */
+package org.xpday.currency;
+
+/**
+ * @author <a href="mailto:damian.guy@thoughtworks.com">Damian Guy</a>
+ *         Date: 15-Nov-2004
+ */
+public class SterlingCurrencyConverter implements CurrencyConverter {
+	private ExchangeRateService exchangeRateService;
+
+	public SterlingCurrencyConverter(ExchangeRateService exchangeRateService) {
+		this.exchangeRateService = exchangeRateService;
+	}
+
+	private double convert(double amount, double rate) throws InvalidAmountException {
+		if (amount < 0) {
+			throw new InvalidAmountException("amount must be non-negative");
+		}
+		return amount * rate;
+	}
+
+	public double convertFromSterling(double amount, Currency toCurrency) throws InvalidAmountException {
+		return convert(amount, exchangeRateService.retrieveRate(toCurrency).fromRate);
+	}
+
+	public double convertToSterling(double amount, Currency fromCurrency) throws InvalidAmountException {
+		return convert(amount, exchangeRateService.retrieveRate(fromCurrency).toRate);
+	}
+
+}
