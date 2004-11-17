b7849d9d5a9b2e55b0808bfd979fbe13e8add080
damian <damian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-17 16:43:28 +0000

    adding behaviour methods for eq constraints

--- trunk/core/behaviour/com/thoughtworks/jbehave/core/minimock/UsingMiniMockBehaviour.java
+++ trunk/core/behaviour/com/thoughtworks/jbehave/core/minimock/UsingMiniMockBehaviour.java
@@ -7,6 +7,8 @@
  */
 package com.thoughtworks.jbehave.core.minimock;
 
+import com.thoughtworks.jbehave.core.Verify;
+
 /**
  * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
  */
@@ -14,9 +16,39 @@ public class UsingMiniMockBehaviour {
     public void shouldStoreMock() throws Exception {
         // given...
         
-
+        Verify.pending("Should Store a mock");
         // expect...
         // when...
         // then...
+    }
+    
+    public void shouldCreateConstraintForPrimitiveDoubleType() throws Exception {
+        UsingMiniMock miniMock = new UsingMiniMock();
+        Constraint c = miniMock.eq(1.0);
+        Verify.that(c.matches(new Double(1.0)));
+    }
+    
+    public void shouldCreateConstraintForPrimitiveIntType() throws Exception {
+        UsingMiniMock miniMock = new UsingMiniMock();
+        Constraint c = miniMock.eq(1);
+        Verify.that(c.matches(new Integer(1)));
+    }
+    
+    public void shouldCreateConstraintForPrimitiveCharType() throws Exception {
+        UsingMiniMock miniMock = new UsingMiniMock();
+        Constraint c = miniMock.eq('c');
+        Verify.that(c.matches(new Character('c')));
+    }
+    
+    public void shouldCreateConstraintForPrimitiveLongType() throws Exception {
+        UsingMiniMock miniMock = new UsingMiniMock();
+        Constraint c = miniMock.eq(1l);
+        Verify.that(c.matches(new Long(1)));
+    }
+    
+    public void shouldCreateConstraintForPrimitiveBooleanType() throws Exception {
+        UsingMiniMock miniMock = new UsingMiniMock();
+        Constraint c = miniMock.eq(true);
+        Verify.that(c.matches(Boolean.TRUE)); 
     }
 }
