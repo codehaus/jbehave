d464b79d3d831562873061919adfacaa1892e65a
damian <damian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-17 20:01:37 +0000

    add behaviours for UsingMiniMock.returnValue(..)

--- trunk/core/behaviour/com/thoughtworks/jbehave/core/minimock/UsingMiniMockBehaviour.java
+++ trunk/core/behaviour/com/thoughtworks/jbehave/core/minimock/UsingMiniMockBehaviour.java
@@ -20,13 +20,19 @@ public class UsingMiniMockBehaviour {
         miniMock = new UsingMiniMock();
     }
     
-    public void shouldStoreMock() throws Exception {
-        // given...
+    interface BehaviourInterface2 {
         
-        Verify.pending("Should Store a mock");
-        // expect...
-        // when...
-        // then...
+    }
+    
+    public void shouldStoreMock() throws Exception {
+        // can't think of a better way of doing it without
+        // exposing the internals!
+        Verify.that(!miniMock.containsMocks());
+        
+        Mock mock = miniMock.mock(BehaviourInterface2.class);
+        
+        Verify.that(miniMock.containsMocks());
+      
     }
     
     public void shouldCreateConstraintForPrimitiveDoubleType() throws Exception {
@@ -71,4 +77,88 @@ public class UsingMiniMockBehaviour {
         Constraint c = miniMock.eq(s);
         Verify.that(c.matches(new Short(s)));
     }
+    
+    interface BehaviourInterface1 {
+        int getInt();
+        long getLong();
+        byte getByte();
+        short getShort();
+        double getDouble();
+        float getFloat();
+        char getChar();
+        boolean getBoolean();
+    }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveInt() throws Exception {
+      Mock mock = miniMock.mock(BehaviourInterface1.class); 
+      mock.stubs("getInt").will(miniMock.returnValue(1));
+   
+      int i = ((BehaviourInterface1)mock).getInt();
+      Verify.equal(1, i);
+    }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveLong() throws Exception {
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getLong").will(miniMock.returnValue(1l));
+     
+        long i = ((BehaviourInterface1)mock).getLong();
+        Verify.equal(1, i);
+    }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveShort() throws Exception {
+        short s = 2;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getShort").will(miniMock.returnValue(s));
+     
+        short i = ((BehaviourInterface1)mock).getShort();
+        Verify.equal(s, i);
+     }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveByte() throws Exception {
+        byte b = 3;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getByte").will(miniMock.returnValue(b));
+     
+        byte i = ((BehaviourInterface1)mock).getByte();
+        Verify.equal(b, i);
+     }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveDouble() throws Exception {
+        double d = 4;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getDouble").will(miniMock.returnValue(d));
+     
+        double i = ((BehaviourInterface1)mock).getDouble();
+        Verify.equal(d, i, 0);
+     }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveFloat() throws Exception {
+        float f = 4;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getFloat").will(miniMock.returnValue(f));
+     
+        float i = ((BehaviourInterface1)mock).getFloat();
+        Verify.equal(f, i, 0);
+     }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveChar() throws Exception {
+        char c = 4;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getChar").will(miniMock.returnValue(c));
+     
+        char i = ((BehaviourInterface1)mock).getChar();
+        Verify.equal(c, i);
+     }
+    
+    public void shouldCreateCorrectReturnValueForPrimitiveBoolean() throws Exception {
+        boolean b = true;
+        Mock mock = miniMock.mock(BehaviourInterface1.class); 
+        mock.stubs("getBoolean").will(miniMock.returnValue(b));
+     
+        boolean i = ((BehaviourInterface1)mock).getBoolean();
+        Verify.equal(b, i);
+     }
+    
+    
+ 
 }
