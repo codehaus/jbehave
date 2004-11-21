22b3a2732d6cca5da23fb54101029f9a6309ee38
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2004-11-21 19:08:19 +0000

    Added listeners to StoryVerifier

--- trunk/extensions/story/src/com/thoughtworks/jbehave/extensions/story/verifier/StoryVerifier.java
+++ trunk/extensions/story/src/com/thoughtworks/jbehave/extensions/story/verifier/StoryVerifier.java
@@ -7,16 +7,23 @@
  */
 package com.thoughtworks.jbehave.extensions.story.verifier;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.thoughtworks.jbehave.core.listener.ResultListener;
 import com.thoughtworks.jbehave.core.visitor.Visitable;
 import com.thoughtworks.jbehave.core.visitor.Visitor;
 import com.thoughtworks.jbehave.extensions.story.domain.Scenario;
 import com.thoughtworks.jbehave.extensions.story.domain.Story;
 import com.thoughtworks.jbehave.extensions.story.invoker.ScenarioInvoker;
+import com.thoughtworks.jbehave.extensions.story.result.ScenarioResult;
 
 /**
  * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
  */
 public class StoryVerifier implements Visitor {
+    private final List listeners = new ArrayList();
     private final ScenarioInvoker scenarioInvoker;
 
     public StoryVerifier(ScenarioInvoker scenarioInvoker) {
@@ -29,7 +36,14 @@ public class StoryVerifier implements Visitor {
 
     public void visit(Visitable visitable) {
         if (visitable instanceof Scenario) {
-            scenarioInvoker.invoke((Scenario) visitable);
+            ScenarioResult result = scenarioInvoker.invoke((Scenario) visitable);
+            for (Iterator i = listeners.iterator(); i.hasNext();) {
+                ((ResultListener)i.next()).gotResult(result);
+            }
         }
+    }
+
+    public void addListener(ResultListener listener) {
+        listeners.add(listener);
     }
 }
