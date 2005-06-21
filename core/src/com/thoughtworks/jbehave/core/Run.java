e59a8884a1d0ba8f4796bef77bb0079015c43ac2
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2005-06-21 09:17:06 +0000

    Run now takes multiple behaviour class parameters

--- core/src/com/thoughtworks/jbehave/core/Run.java
+++ core/src/com/thoughtworks/jbehave/core/Run.java
@@ -7,6 +7,7 @@
  */
 package com.thoughtworks.jbehave.core;
 
+import java.io.PrintStream;
 import java.io.PrintWriter;
 
 import com.thoughtworks.jbehave.core.behaviour.BehaviourClass;
@@ -19,25 +20,38 @@ import com.thoughtworks.jbehave.core.listener.PlainTextMethodListener;
  */
 public class Run {
     public static void main(String[] args) {
-        try {
-            Class classToVerify = Class.forName(args[0]);
-            BehaviourClass visitableClass = new BehaviourClass(classToVerify);
-            PlainTextMethodListener textListener = new PlainTextMethodListener(new PrintWriter(System.out));
-            BehaviourMethodVerifier verifier = new BehaviourMethodVerifier();
-            verifier.addListener(textListener);
-            visitableClass.accept(verifier);
-            textListener.printReport();
-            if (textListener.hasBehaviourFailures()) {
-                System.exit(1);
-            }
-        } catch (Exception e) {
-            System.err.println("Problem verifying behaviour class " + args[0]);
-            e.printStackTrace();
-            System.exit(1);
-        } catch (JBehaveFrameworkError e) {
-            System.err.println("Problem verifying behaviour class " + args[0]);
-            e.printStackTrace();
-            System.exit(1);
-        }
+    	runBehaviourToStream(args, System.out);
     }
+    
+    public static void runBehaviourToStream(String[] args, PrintStream printStream) {
+    	boolean behavesAsExpected = true;
+    	for (int i = 0; i < args.length; i++) {
+	        try {
+				PlainTextMethodListener textListener = runBehaviour(args[i], printStream);
+	            textListener.printReport();
+	            if (textListener.hasBehaviourFailures()) {
+	                behavesAsExpected = false;
+	            }
+	        } catch (Exception e) {
+	            System.err.println("Problem verifying behaviour class " + args[0]);
+	            e.printStackTrace();
+	            System.exit(1);
+	        } catch (JBehaveFrameworkError e) {
+	            System.err.println("Problem verifying behaviour class " + args[0]);
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+    	}
+    	if (!behavesAsExpected) {System.exit(1);}
+    }
+
+	private static PlainTextMethodListener runBehaviour(String behaviourClassName, PrintStream printStream) throws ClassNotFoundException {
+		Class classToVerify = Class.forName(behaviourClassName);
+		BehaviourClass visitableClass = new BehaviourClass(classToVerify);
+		PlainTextMethodListener textListener = new PlainTextMethodListener(new PrintWriter(printStream));
+		BehaviourMethodVerifier verifier = new BehaviourMethodVerifier();
+		verifier.addListener(textListener);
+		visitableClass.accept(verifier);
+		return textListener;
+	}
 }
