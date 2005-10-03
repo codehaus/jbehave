85328b7f53241c8a64ba3cd969fad2af4967e3a1
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2005-10-03 19:38:56 +0000

    DN: Renamed BehaviourClassContainer to Behaviours, did some tidying up

--- extensions/ant/behaviour/com/thoughtworks/jbehave/ant/AllBehaviourClasses.java
+++ extensions/ant/behaviour/com/thoughtworks/jbehave/ant/AllBehaviourClasses.java
@@ -7,12 +7,12 @@
  */
 package com.thoughtworks.jbehave.ant;
 
-import com.thoughtworks.jbehave.core.behaviour.BehaviourClassContainer;
+import com.thoughtworks.jbehave.core.behaviour.Behaviours;
 
 /**
  * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
  */
-public class AllBehaviourClasses implements BehaviourClassContainer {
+public class AllBehaviourClasses implements Behaviours {
 
     public Class[] getBehaviourClasses() {
         return new Class[] {
