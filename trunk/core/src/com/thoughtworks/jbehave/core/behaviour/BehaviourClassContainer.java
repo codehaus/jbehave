15d1502a40567ee5aa43d1184082db61e0e88978
tastapod <tastapod@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2005-10-05 16:48:51 +0000

    DN: oops! BehaviourClassContainer was supposed to extend Behaviours

--- trunk/core/src/com/thoughtworks/jbehave/core/behaviour/BehaviourClassContainer.java
+++ trunk/core/src/com/thoughtworks/jbehave/core/behaviour/BehaviourClassContainer.java
@@ -3,6 +3,6 @@ package com.thoughtworks.jbehave.core.behaviour;
 /**
  * @deprecated renamed to {@link Behaviours}
  */
-public interface BehaviourClassContainer {
+public interface BehaviourClassContainer extends Behaviours {
 
 }
