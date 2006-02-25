3f97b179f7abb97ed75a3f1d0840dfeb6880db79
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-02-25 08:16:59 +0000

    Fixing StackOverflowError in UsingConstraints' long / int matching - and learning never to write even simple code without tests, especially if it's repetitive.

---
+++ trunk/core/behaviour/jbehave/core/UsingConstraintsBehaviour.java
@@ -0,0 +1,31 @@
+package jbehave.core;
+
+import jbehave.core.minimock.UsingConstraints;
+
+public class UsingConstraintsBehaviour extends UsingConstraints {
+
+    public void shouldProvideConstraintsForDoubles() {
+        ensureThat(5.0, eq(5.0));
+        ensureThat(5.0, not(eq(5.1)));
+        ensureThat(5.0, eq(5.0), "message");
+    }
+    
+    public void shouldProvideConstraintsForLongsAndInts() {
+        ensureThat(5, eq(5));
+        ensureThat(5, not(eq(4)));
+        ensureThat(5, eq(5), "message");
+    }
+    
+    public void shouldProvideConstraintsForChars() {
+        ensureThat('a', eq('a'));
+        ensureThat('a', not(eq('A')));
+        ensureThat('a', eq('a'), "message");
+    }
+    
+    public void shouldProvideConstraintsForBooleans() {
+
+        ensureThat(true, eq(true));
+        ensureThat(true, not(eq(false)));
+        ensureThat(true, eq(true), "message");
+    }
+}
