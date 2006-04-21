e09a99fce5b3cc945eab6863f8ea3c1872aa2072
cli <cli@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-04-21 15:38:12 +0000

    Issue No:JBEHAVE-15:added eclipse plugin

---
+++ trunk/plugins/eclipse/src/main/java/jbehave/plugin/eclipse/JBehaveLaunchConfiguration.java
@@ -0,0 +1,105 @@
+package jbehave.plugin.eclipse;
+
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.internal.launching.LaunchingPlugin;
+import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.ExecutionArguments;
+import org.eclipse.jdt.launching.IVMRunner;
+import org.eclipse.jdt.launching.VMRunnerConfiguration;
+
+public class JBehaveLaunchConfiguration extends AbstractJavaLaunchConfigurationDelegate {
+	public static final String ATTR_BEHAVIOR_CLASS = LaunchingPlugin.getUniqueIdentifier() + ".BEHAVIOR_CLASS"; //$NON-NLS-1$
+	public static final String ID_JBEHAVE_APPLICATION = "jbehave.plugin.eclipse.launchconfig";
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		
+		if (monitor == null) {
+			monitor = new NullProgressMonitor();
+		}
+		
+		monitor.beginTask(MessageFormat.format("{0}...", new String[]{configuration.getName()}), 3); //$NON-NLS-1$
+		// check for cancellation
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		monitor.subTask("Building configuration..."); //$NON-NLS-1$
+						
+		String mainTypeName = verifyMainTypeName(configuration);
+		IVMRunner runner = getVMRunner(configuration, mode);
+
+		File workingDir = verifyWorkingDirectory(configuration);
+		String workingDirName = null;
+		if (workingDir != null) {
+			workingDirName = workingDir.getAbsolutePath();
+		}
+		
+		// Environment variables
+		String[] envp= getEnvironment(configuration);
+		
+		// Program & VM args
+		String pgmArgs = getBehaviorClass(configuration);
+		String vmArgs = getVMArguments(configuration);
+		ExecutionArguments execArgs = new ExecutionArguments(vmArgs, pgmArgs);
+		
+		// VM-specific attributes
+		Map vmAttributesMap = getVMSpecificAttributesMap(configuration);
+		
+		// Classpath
+		String[] classpath = getClasspath(configuration);
+		
+		// Create VM config
+		VMRunnerConfiguration runConfig = new VMRunnerConfiguration(mainTypeName, classpath);
+		runConfig.setProgramArguments(execArgs.getProgramArgumentsArray());
+		runConfig.setEnvironment(envp);
+		runConfig.setVMArguments(execArgs.getVMArgumentsArray());
+		runConfig.setWorkingDirectory(workingDirName);
+		runConfig.setVMSpecificAttributesMap(vmAttributesMap);
+
+		// Bootpath
+		runConfig.setBootClassPath(getBootpath(configuration));
+		
+		// check for cancellation
+		if (monitor.isCanceled()) {
+			return;
+		}		
+		
+		// stop in main
+		prepareStopInMain(configuration);
+		
+		// done the verification phase
+		monitor.worked(1);
+		
+		monitor.subTask("Launching..."); //$NON-NLS-1$
+		// set the default source locator if required
+		setDefaultSourceLocator(launch, configuration);
+		monitor.worked(1);		
+		
+		// Launch the configuration - 1 unit of work
+		runner.run(runConfig, launch, monitor);
+		
+		// check for cancellation
+		if (monitor.isCanceled()) {
+			return;
+		}	
+		
+		monitor.done();
+	}
+
+	private String getBehaviorClass(ILaunchConfiguration configuration) throws CoreException {
+		return configuration.getAttribute(
+				ATTR_BEHAVIOR_CLASS, ""); //$NON-NLS-1$
+}
+
+}
