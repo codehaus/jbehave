47b26ee0b8c235d2472c6f2e62a3fc30e9271bdc
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-09-23 17:45:38 +0000

    Added injection of ClassLoader to BehavioursLoader.

--- trunk/core/src/behaviour/jbehave/core/util/BehavioursLoaderBehaviour.java
+++ trunk/core/src/behaviour/jbehave/core/util/BehavioursLoaderBehaviour.java
@@ -1,18 +1,40 @@
 package jbehave.core.util;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import jbehave.core.minimock.Constraint;
 import jbehave.core.minimock.MockObjectBehaviour;
 import jbehave.core.minimock.UsingConstraints;
 import jbehave.core.story.listener.PlainTextListenerBehaviour;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.lang.reflect.Modifier;
-
 public class BehavioursLoaderBehaviour extends UsingConstraints {
+
     public void shouldLoadPublicBehaviourEndedWithBehaviourButNotAbstract() throws Exception {
         Set loadedFromBehaviourLoader = new HashSet(Arrays.asList(new BehavioursLoader(getClass()).loadBehaviours()));
+        ensureThatBehavioursAreLoaded(loadedFromBehaviourLoader);
+    }
+
+    public void shouldLoadBehaviourUsingCustomClassLoader() throws Exception {
+        BehavioursLoader loader = new BehavioursLoader(getClass(), new CustomClassLoader(false));
+        Set loadedFromBehaviourLoader = new HashSet(Arrays.asList(loader.loadBehaviours()));
+        ensureThatBehavioursAreLoaded(loadedFromBehaviourLoader);
+    }
+
+    public void shouldNotLoadBehaviourUsingInvalidCustomClassLoader() throws Exception {
+        BehavioursLoader loader = new BehavioursLoader(getClass(), new CustomClassLoader(true));
+        Set loadedFromBehaviourLoader = new HashSet();
+        try {
+            loadedFromBehaviourLoader = new HashSet(Arrays.asList(loader
+                    .loadBehaviours()));
+        } catch ( Exception e) {
+            ensureThat(loadedFromBehaviourLoader, setEmpty());
+        }        
+    }
+    
+    private void ensureThatBehavioursAreLoaded(Set loadedFromBehaviourLoader) {
         ensureThat(loadedFromBehaviourLoader, setContains(MockObjectBehaviour.class));
         ensureThat(Modifier.isAbstract(PlainTextListenerBehaviour.class.getModifiers()), eq(true));
         ensureThat(loadedFromBehaviourLoader, not(setContains(PlainTextListenerBehaviour.class)));
@@ -28,5 +50,34 @@ public class BehavioursLoaderBehaviour extends UsingConstraints {
                 return "collection contains <" + item + ">";
             }
         };
+    }
+
+    private Constraint setEmpty() {
+        return new Constraint() {
+            public boolean matches(Object arg) {
+                return ((Set) arg).isEmpty();
+            }
+
+            public String toString() {
+                return "collection is empty";
+            }
+        };
+    }
+
+    
+    private static class CustomClassLoader extends ClassLoader {
+        
+        boolean invalid;
+        
+        public CustomClassLoader(boolean invalid) {
+            this.invalid = invalid;
+        }
+
+        public Class findClass(String name) throws ClassNotFoundException{
+            if ( invalid ){
+                throw new ClassNotFoundException();
+            }
+            return Class.forName(name);            
+        }
     }
 }
