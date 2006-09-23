81764cd9cc3f3b2bc952d1ac80a928a4b84e090c
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-09-23 17:22:58 +0000

    Added injection of ClassLoader to core Run.

--- core/src/java/jbehave/core/Run.java
+++ core/src/java/jbehave/core/Run.java
@@ -10,22 +10,47 @@ import jbehave.core.util.Timer;
 
 /**
  * This is the entry point to run one or more {@link BehaviourClass}.
+ * Classes are loaded using the ClassLoader injected, which defaults to the current context
+ * ClassLoader.
  * 
  * @author <a href="mailto:dan.north@thoughtworks.com">Dan North</a>
+ * @author Mauro Talevi
  */
 public class Run {
     private boolean succeeded = true;
     private final PrintWriter writer;
+    private ClassLoader classLoader;
 
     public Run(PrintStream out) {
+        this(out, Thread.currentThread().getContextClassLoader());
+    }
+    
+    public Run(PrintStream out, ClassLoader classLoader) {
         this.writer = new PrintWriter(out);
+        this.classLoader = classLoader;
     }
 
+    public boolean succeeded() {
+        return succeeded;
+    }
+
+    public void verifyBehaviour(String classNameToVerify) throws ClassNotFoundException {
+        verifyBehaviour(classLoader.loadClass(classNameToVerify));
+    }
+    
+    public void verifyBehaviour(Class classToVerify) {
+        PlainTextListener textListener = new PlainTextListener(new PrintWriter(writer), new Timer());
+        BehaviourVerifier verifier = new BehaviourVerifier(textListener);
+        verifier.verifyBehaviour(new BehaviourClass(classToVerify, verifier));
+        textListener.printReport();
+        succeeded = succeeded && !textListener.hasBehaviourFailures();
+    }
+    
     public static void main(String[] args) {
         try {
             Run run = new Run(System.out);
             for (int i = 0; i < args.length; i++) {
-                run.verifyBehaviour(Class.forName(args[i]));
+                run.verifyBehaviour(args[i]);
             }
             System.exit(run.succeeded() ? 0 : 1);
         } catch (ClassNotFoundException e) {
@@ -34,15 +59,5 @@ public class Run {
         }
     }
 
-    public boolean succeeded() {
-        return succeeded;
-    }
 
-    public void verifyBehaviour(Class classToVerify) {
-        PlainTextListener textListener = new PlainTextListener(new PrintWriter(writer), new Timer());
-        BehaviourVerifier verifier = new BehaviourVerifier(textListener);
-        verifier.verifyBehaviour(new BehaviourClass(classToVerify, verifier));
-        textListener.printReport();
-        succeeded = succeeded && !textListener.hasBehaviourFailures();
-    }
 }
