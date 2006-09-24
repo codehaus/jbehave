bcdb5e5e8e540e0c263b2908e469c9776ce5c549
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-09-24 10:11:48 +0000

    [EK] Fixed JBehaveTaskBehaviour for 1.4; String.contains(...) is a Java 5 addition

--- trunk/extensions/ant/src/behaviour/jbehave/ant/JBehaveTaskBehaviour.java
+++ trunk/extensions/ant/src/behaviour/jbehave/ant/JBehaveTaskBehaviour.java
@@ -53,7 +53,7 @@ public class JBehaveTaskBehaviour extends UsingMiniMock {
 
         ensureThat(runner.taskLog, sameInstanceAs(task));
         String[] actualCommand = runner.commandLineLog;
-        ensureThat(actualCommand[0].contains("java"), eq(true));
+        ensureThat(actualCommand[0].matches(".*java.*"), eq(true));
         List list = Arrays.asList(actualCommand);
         ensureThat(list, collectionContains(BehaviourClassOne.class.getName()));
     }
