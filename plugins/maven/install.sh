b57cca78008e91f8ed8b1ec8442c971b60483183
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-11-18 19:35:34 +0000

    Added initial maven plugin for jbehave.

---
+++ plugins/maven/install.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=jbehave -DartifactId=jbehave -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=../../delete_me/1.0-SNAPSHOT/jbehave.jar                                   
