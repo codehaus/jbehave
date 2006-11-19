d40b381b6b166399df908fc4110949021ac99596
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-11-19 12:57:54 +0000

    Added poms and script to deploy jbehave artifact built with ant, so that it can be used by maven.

--- plugins/maven/install.sh
+++ plugins/maven/install.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=jbehave -DartifactId=jbehave -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=../../delete_me/1.0-SNAPSHOT/jbehave.jar                                   
+mvn -U install:install-file -DgroupId=jbehave -DartifactId=jbehave -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=../../delete_me/1.0-SNAPSHOT/jbehave.jar                                   
