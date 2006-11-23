9bf60b47a55874b2797a2bc50b4f60b917fa6938
wolfdancer <wolfdancer@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-11-23 09:08:18 +0000

    [JBEHAVE-74] website updated to use BuildMaster 0.9 and added instruction on how to update website

--- trunk/sitespec.rb
+++ trunk/sitespec.rb
@@ -3,14 +3,17 @@ require 'buildmaster'
 
 class MySiteSpec < BuildMaster::SiteSpec
   def initialize
-    root = File.dirname(__FILE__)
-    @output_dir = File.join(root, 'website', 'output')
-    @content_dir = File.join(root, 'website', 'content')
-    @template_file = File.join(root, 'website', 'templates', 'skin.html')
+    root = BuildMaster::Cotta.new.file(__FILE__).parent
+    @output_dir = root.dir('website/output')
+    @content_dir = root.dir('website/content')
+    @template_file = root.file('website/templates/skin.html')
+    properties['release'] = '0.9'
+    properties['prerelease'] = '0.9'
+    properties['snapshot'] = '0.9'
   end
 
   def index_file?(content_path)
-    return content_path =~ /index/
+    return content_path.to_s =~ /index/
   end
   
   def not_index_file?(content_path)
