e7008efd75b7fe781c1ebb9e0e4e7abc53a8c344
felix <felix@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-11-30 12:16:26 +0000

    [FL] Improved class names. extracted "parsing". introduced outline page.

--- trunk/plugins/eclipse/src/main/java/jbehave/plugin/eclipse/actions/NavigateToJavaSource.java
+++ trunk/plugins/eclipse/src/main/java/jbehave/plugin/eclipse/actions/NavigateToJavaSource.java
@@ -1,11 +1,10 @@
 package jbehave.plugin.eclipse.actions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-import jbehave.plugin.eclipse.editors.BehaviourEditor;
+import jbehave.plugin.eclipse.editors.StoryEditor;
+import jbehave.plugin.eclipse.model.StoryLine;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IType;
@@ -17,7 +16,6 @@ import org.eclipse.jdt.core.search.SearchMatch;
 import org.eclipse.jdt.core.search.SearchParticipant;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.core.search.SearchRequestor;
-import org.eclipse.jdt.core.search.TypeNameRequestor;
 import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.BadLocationException;
@@ -26,31 +24,20 @@ import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.INavigationLocation;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.TextSelectionNavigationLocation;
 
 public class NavigateToJavaSource implements IEditorActionDelegate {
 
-	List keyWords=Arrays.asList(new String[]{"title:","scenario:","when","then","given"});
-	private BehaviourEditor editor;
+	
+	private StoryEditor editor;
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		this.editor=(BehaviourEditor)targetEditor;
+		this.editor=(StoryEditor)targetEditor;
 		
 	}
 	
-	 public static String capitalize(String str) {
-	        int strLen;
-	        if (str == null || (strLen = str.length()) == 0) {
-	            return str;
-	        }
-	        return new StringBuffer(strLen)
-	            .append(Character.toTitleCase(str.charAt(0)))
-	            .append(str.substring(1))
-	            .toString();
-	    }
+	
 
 
 	public void run(IAction action) {
@@ -61,42 +48,18 @@ public class NavigateToJavaSource implements IEditorActionDelegate {
 		int line=selection.getStartLine();
 		try {
 			String text=document.get(document.getLineOffset(line),document.getLineLength(line));
-			processLine(text);
+			jbehave.plugin.eclipse.model.StoryLine storyLine = StoryLine.parseLine(text);
+			if (storyLine!=null){
+				findAndOpenClass(storyLine.asClassName());
+			}
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void processLine(String line) {
-		line=line.replaceAll("\n", "");
-		line=line.replaceAll("\r", "");
-		List words=new ArrayList(Arrays.asList(line.split(" ")));
-		if (words.size()<2)
-			return;
-		
-		if (((String)words.get(0)).equalsIgnoreCase("and")){
-			words.remove(0);
-			if (words.size()<2)
-				return;
-		}
-		
-		if (keyWords.contains(((String)words.get(0)).toLowerCase())){	
-			words.remove(0);
-			String className = renderClassName(words);
-			findAndOpenClass(className);
-		}
-		
-	}
+	
 
-	private String renderClassName(List words) {
-		String className="";
-		for (Iterator iter = words.iterator(); iter.hasNext();) {
-			String word = (String) iter.next();
-			className+=capitalize(word);				
-		}
-		System.out.println("Class: '"+className+"'");
-		return className;
-	}
+	
 
 	private void findAndOpenClass(String className) {
 		try {
