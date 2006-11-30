373b7fdfcb07df1ddc4af1e316055962bb6968ff
felix <felix@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2006-11-30 15:28:46 +0000

    [FL] F3 if no class is found, bring up NewClassWizard, if classname is ambiguous bring up choice.

--- trunk/plugins/eclipse/src/main/java/jbehave/plugin/eclipse/actions/NavigateToJavaSource.java
+++ trunk/plugins/eclipse/src/main/java/jbehave/plugin/eclipse/actions/NavigateToJavaSource.java
@@ -17,27 +17,39 @@ import org.eclipse.jdt.core.search.SearchParticipant;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.core.search.SearchRequestor;
 import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.SelectionDialog;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.TextSelectionNavigationLocation;
 
+import wizzards.NewClassWizard;
+
 public class NavigateToJavaSource implements IEditorActionDelegate {
 
-	
+
 	private StoryEditor editor;
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
 		this.editor=(StoryEditor)targetEditor;
-		
+
 	}
-	
-	
+
+
 
 
 	public void run(IAction action) {
@@ -50,19 +62,20 @@ public class NavigateToJavaSource implements IEditorActionDelegate {
 			String text=document.get(document.getLineOffset(line),document.getLineLength(line));
 			jbehave.plugin.eclipse.model.StoryLine storyLine = StoryLine.parseLine(text);
 			if (storyLine!=null){
-				findAndOpenClass(storyLine.asClassName());
+				findAndOpenClass(storyLine);
 			}
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
 	}
 
-	
 
-	
 
-	private void findAndOpenClass(String className) {
+
+
+	private void findAndOpenClass(StoryLine storyLine) {
 		try {
+			String className=storyLine.asClassName();
 			final List types = new ArrayList();
 		    SearchPattern pattern = SearchPattern.createPattern(className, IJavaSearchConstants.TYPE, IJavaSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
 		    IJavaSearchScope scope = SearchEngine.createWorkspaceScope();
@@ -75,16 +88,22 @@ public class NavigateToJavaSource implements IEditorActionDelegate {
 
 		    SearchEngine searchEngine = new SearchEngine();
 		    searchEngine.search(
-		    		pattern, 
-		    		new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()}, 
-		    		scope, 
-		    		requestor, 
+		    		pattern,
+		    		new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()},
+		    		scope,
+		    		requestor,
 		    		null);
 				if (types.size()==1){
-					EditorUtility.openInEditor((IType)types.get(0), true);
-				}
-			
-		} catch (JavaModelException e) {
+                    openInEditor((IType) types.get(0));
+                }
+				if (types.size()==0){
+                    bringUpNewClassWizard(storyLine);
+                }
+			    if (types.size()>1){
+                    openInEditor(selectType(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),className));
+                }
+
+        } catch (JavaModelException e) {
 			throw new RuntimeException(e);
 		} catch (CoreException e) {
 			throw new RuntimeException(e);
@@ -92,12 +111,48 @@ public class NavigateToJavaSource implements IEditorActionDelegate {
 		
 	}
 
+    public IType selectType(Shell parent, String filter) throws JavaModelException {
+        SelectionDialog dialog= JavaUI.createTypeDialog(
+            parent, new ProgressMonitorDialog(parent),
+            SearchEngine.createWorkspaceScope(),
+            IJavaElementSearchConstants.CONSIDER_ALL_TYPES, false,
+            filter
 
-	
+       );
+        dialog.setTitle("Choose Class");
+        dialog.setMessage("");
+        if (dialog.open() == IDialogConstants.CANCEL_ID)
+            return null;
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
+        Object[] types= dialog.getResult();
+        if (types == null || types.length == 0)
+            return null;
+        return (IType)types[0];
+    }
 
-	}
+    private void openInEditor(IType type) throws JavaModelException, PartInitException {
+        EditorUtility.openInEditor(type, true);
+    }
+
+    private void bringUpNewClassWizard( StoryLine storyLine) {
+        Shell shell= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+        MessageDialog dialog=new MessageDialog(null,"Type "+storyLine.asClassName()+" not found",null,"Do you want to create it?",
+                                                MessageDialog.QUESTION,
+                                                new String[]{"Yes","Yes, using Minimock","No"},0);
+        int ret=dialog.open();
+        if ((ret==0)||(ret==1)){
+            NewClassWizard wizard=new NewClassWizard(storyLine,ret==1);
+            wizard.init(PlatformUI.getWorkbench(), null);
+            WizardDialog wd=new WizardDialog(shell,wizard);
+
+            wd.open();
+        }
+    }
+
+
+    public void selectionChanged(IAction action, ISelection selection) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
