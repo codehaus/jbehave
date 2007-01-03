af69118aea301e84955281e71b14fd71507db79f
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-01-03 11:12:18 +0000

    [EK] New Story: The Player Loses the Game

--- trunk/examples/hellbound/src/stories/com/sirenian/hellbound/outcomes/TheNextGlyphShouldAppear.java
+++
@@ -1,9 +0,0 @@
-package com.sirenian.hellbound.outcomes;
-
-import org.jbehave.core.story.domain.World;
-
-
-public class TheNextGlyphShouldAppear extends HellboundOutcome {
-    public void verifyAnyTimeIn(World world) {
-    }
-}
---
+++ trunk/examples/hellbound/src/stories/com/sirenian/hellbound/scenarios/TheGameShouldBeOver.java
@@ -0,0 +1,26 @@
+package com.sirenian.hellbound.scenarios;
+
+import javax.swing.JLabel;
+
+import org.jbehave.core.Ensure;
+import org.jbehave.core.story.domain.World;
+import org.jbehave.threaded.swing.WindowWrapper;
+
+import com.sirenian.hellbound.gui.ComponentNames;
+import com.sirenian.hellbound.outcomes.HellboundOutcome;
+import com.sirenian.hellbound.stories.WorldKey;
+
+public class TheGameShouldBeOver extends HellboundOutcome {
+
+    protected void verifyAnyTimeIn(World world) {
+        WindowWrapper windowWrapper = (WindowWrapper) world.get(WorldKey.WINDOW_WRAPPER);
+        JLabel messageLabel = null;
+        try {
+             messageLabel = (JLabel) windowWrapper.findComponent(ComponentNames.GAME_MESSAGE);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Ensure.that(messageLabel.getText(), eq("Game over, man! Game over!"));
+    }
+
+}
