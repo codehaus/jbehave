b0240c6901c90a29393a24e51ab5a92462d28453
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-01-04 15:17:38 +0000

    Repackaged maven plugin to use org.jbehave.*

--- trunk/plugins/maven/src/it/src/main/java/org/jbehave/it/ItBehaviours.java
+++ trunk/plugins/maven/src/it/src/main/java/org/jbehave/it/ItBehaviours.java
@@ -1,12 +1,12 @@
-package jbehave.it;
+package org.jbehave.it;
 
-import jbehave.core.behaviour.Behaviours;
+import org.jbehave.core.behaviour.Behaviours;
 
 public class ItBehaviours implements Behaviours {
 
     public Class[] getBehaviours() {
         return new Class[] {
-                jbehave.it.SampleBehaviour.class
+                org.jbehave.it.SampleBehaviour.class
         };
     }
 }
