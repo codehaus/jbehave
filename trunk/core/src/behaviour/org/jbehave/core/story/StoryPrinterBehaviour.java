99f5fcec4633f26b7021f252b82e31425dccafd5
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-02-26 17:58:54 +0000

    Added StoryLoader to abstract the loading of a Story - both via StoryParser and from class name. Refactored StoryPrinter to have StoryLoader and Renderer as injectable dependendencies.

--- trunk/core/src/behaviour/org/jbehave/core/story/StoryPrinterBehaviour.java
+++ trunk/core/src/behaviour/org/jbehave/core/story/StoryPrinterBehaviour.java
@@ -4,16 +4,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.jbehave.core.mock.UsingMatchers;
+import org.jbehave.core.story.codegen.parser.TextStoryParser;
+import org.jbehave.core.story.renderer.PlainTextRenderer;
 
 public class StoryPrinterBehaviour extends UsingMatchers {
-    
-    
+        
     public void shouldSpecifyAndPrintStoryAsPlainText() throws Exception {
         
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(byteStream);
         
-        StoryPrinter printer = new StoryPrinter(printStream);        
+        StoryPrinter printer = new StoryPrinter(
+                new StoryLoader(new TextStoryParser(), Thread.currentThread().getContextClassLoader()),
+                new PlainTextRenderer(System.out));
         printer.print(SimpleStory.class.getName());
         String result = byteStream.toString();
         
--- trunk/plugins/maven/src/main/java/org/jbehave/mojo/StoryRunnerMojo.java
+++ trunk/plugins/maven/src/main/java/org/jbehave/mojo/StoryRunnerMojo.java
@@ -1,18 +1,12 @@
 package org.jbehave.mojo;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.jbehave.core.story.StoryBuilder;
+import org.jbehave.core.story.StoryLoader;
 import org.jbehave.core.story.StoryRunner;
-import org.jbehave.core.story.codegen.domain.StoryDetails;
 import org.jbehave.core.story.codegen.parser.StoryParser;
 import org.jbehave.core.story.codegen.parser.TextStoryParser;
 import org.jbehave.core.story.domain.Story;
@@ -55,28 +49,13 @@ public class StoryRunnerMojo  extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             getLog().debug("Running story "+ storyPath);
-            Story story = buildStory(storyPath, storyPackage);            
+            StoryLoader loader = new StoryLoader(storyParser, new BehavioursClassLoader(classpathElements));
+            Story story = loader.loadStory(storyPath, storyPackage);            
             storyRunner.run(story);
         } catch (Exception e) {
             throw new MojoExecutionException("Failed to run story "+storyPath+" with package "+storyPackage, e);
         }
     }
 
-    private Story buildStory(String storyPath, String storyPackage) throws MalformedURLException {
-        StoryDetails storyDetails = storyParser.parseStory(getReader(storyPath));
-        return new StoryBuilder(storyDetails, storyPackage).story();
-    }
-
-    private Reader getReader(String resource) throws MalformedURLException {
-        BehavioursClassLoader cl = new BehavioursClassLoader(classpathElements);
-        return getReader(resource, cl);
-    }
-
-    protected Reader getReader(String resource, ClassLoader classLoader) {
-        InputStream is = classLoader.getResourceAsStream(resource);
-        if ( is == null ){
-            throw new NoSuchElementException("Resource "+resource+" not found in ClassLoader "+classLoader.getClass());
-        }
-        return new InputStreamReader(is);
-    }
+  
 }
