06b0a7825075acf4a388c6b0dd97d21010369bb8
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-05-14 19:58:06 +0000

    [EK] More collection constraints

--- core/src/behaviour/org/jbehave/core/matchers/UsingCollectionMatchersBehaviour.java
+++ core/src/behaviour/org/jbehave/core/matchers/UsingCollectionMatchersBehaviour.java
@@ -4,38 +4,64 @@ import java.util.ArrayList;
 
 import org.jbehave.core.Ensure;
 import org.jbehave.core.exception.VerificationException;
+import org.jbehave.core.mock.Matcher;
 import org.jbehave.core.mock.UsingMatchers;
 
 public class UsingCollectionMatchersBehaviour {
     private static final String NL = System.getProperty("line.separator");
 
+    private Matcher eq(Integer value) {
+    	return UsingEqualityMatchers.eq(value);
+	}
+
+	private Matcher not(Matcher matcher) {
+		return UsingLogicalMatchers.not(matcher);
+	}
+    
     public void shouldProvideMatchersForCollectionsContainingAThing() {
         ArrayList list = new ArrayList();
-        list.add(new Integer(3));
+        list.add(Integer.valueOf(3));
         
-        Ensure.that(list, UsingCollectionMatchers.collectionContaining(new Integer(3)));
-        Ensure.that(list, UsingCollectionMatchers.collectionContaining(UsingEqualityMatchers.eq(new Integer(3))));
+        Ensure.that(list, UsingCollectionMatchers.collectionContaining(Integer.valueOf(3)));
+        Ensure.that(list, UsingCollectionMatchers.collectionContaining(eq(Integer.valueOf(3))));
         
-        Ensure.that(list, UsingLogicalMatchers.not(UsingCollectionMatchers.collectionContaining(new Integer(5))));
-        Ensure.that(list, UsingLogicalMatchers.not(UsingCollectionMatchers.collectionContaining(UsingEqualityMatchers.eq(new Integer(5)))));
+        Ensure.that(list, not(UsingCollectionMatchers.collectionContaining(Integer.valueOf(5))));
+        Ensure.that(list, UsingLogicalMatchers.not(UsingCollectionMatchers.collectionContaining(eq(Integer.valueOf(5)))));
+    }
+
+	public void shouldProvideMatchersForCollectionsContainingSomeThings() {
+        ArrayList list = new ArrayList();
+        list.add(Integer.valueOf(3));
+        list.add(Integer.valueOf(4));
+        list.add(Integer.valueOf(5));
+    	
+    	Ensure.that(list, UsingLogicalMatchers.not(UsingCollectionMatchers.collectionContaining(Integer.valueOf(7))));
+    	Ensure.that(list, UsingCollectionMatchers.collectionContaining(Integer.valueOf(3), Integer.valueOf(4)));
+    	Ensure.that(list, UsingCollectionMatchers.collectionContaining(Integer.valueOf(5), Integer.valueOf(4), Integer.valueOf(3)));
+        Ensure.that(list, UsingCollectionMatchers.collectionContaining(new Object[] {Integer.valueOf(3)}));
+       
+    	Ensure.that(list, not(UsingCollectionMatchers.collectionContaining(eq(Integer.valueOf(7)))));
+    	Ensure.that(list, UsingCollectionMatchers.collectionContaining(eq(Integer.valueOf(3)), eq(Integer.valueOf(4))));
+    	Ensure.that(list, UsingCollectionMatchers.collectionContaining(eq(Integer.valueOf(5)), eq(Integer.valueOf(4)), eq(Integer.valueOf(3))));
+        Ensure.that(list, UsingCollectionMatchers.collectionContaining(new Matcher[] {eq(Integer.valueOf(3))}));
     }
     
     public void shouldDescribeMatchersForCollections() {
         UsingMatchers m = new UsingMatchers() {};
         
         ArrayList list = new ArrayList();
-        list.add(new Integer(5));
+        list.add(Integer.valueOf(5));
         
         try {
-            Ensure.that(list, UsingCollectionMatchers.collectionContaining(new Integer(3)));
+            Ensure.that(list, UsingCollectionMatchers.collectionContaining(Integer.valueOf(3)));
         } catch (VerificationException e) {
             Ensure.that(e.getMessage(), UsingEqualityMatchers.eq("Expected: " + NL + "a collection containing [equal to <3>]" + NL + "but got: " + NL + "[5]"));
         }
         
-        list.add(new Integer(6));
+        list.add(Integer.valueOf(6));
         
         try {
-            Ensure.that(list, UsingCollectionMatchers.collectionContaining(new Integer(4), new Integer(5)));
+            Ensure.that(list, UsingCollectionMatchers.collectionContaining(Integer.valueOf(4), Integer.valueOf(5)));
         } catch (VerificationException e) {
             Ensure.that(e.getMessage(), UsingEqualityMatchers.eq("Expected: " + NL + "a collection containing [equal to <4>, equal to <5>]" + NL + "but got: " + NL + "[5, 6]"));
         }
