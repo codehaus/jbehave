3eba61077694ce9b9d77af1a84593a64d7bc93a7
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-07-11 12:57:51 +0000

    JBEHAVE-91: Refactored story mojos to print and run a list of stories. The configuration still supports the specification of a single story and configuration of multiple stories will be optional - and still to be added.

--- trunk/plugins/maven/src/main/java/org/jbehave/mojo/StoryRunnerMojo.java
+++ trunk/plugins/maven/src/main/java/org/jbehave/mojo/StoryRunnerMojo.java
@@ -1,6 +1,10 @@
 package org.jbehave.mojo;
 
 
+import java.net.MalformedURLException;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.jbehave.core.story.StoryLoader;
@@ -8,7 +12,7 @@ import org.jbehave.core.story.StoryRunner;
 import org.jbehave.core.story.domain.Story;
 
 /**
- * Mojo to run a story 
+ * Mojo to run stories 
  * 
  * @author Mauro Talevi
  * @goal run-story
@@ -19,16 +23,24 @@ public class StoryRunnerMojo extends AbstractStoryMojo {
     private StoryRunner storyRunner = new StoryRunner();
     
     public void execute() throws MojoExecutionException, MojoFailureException {
+        String storyPackage = getStoryPackage();
+        List storyPaths = getStoryPaths();
         try {
-            getLog().debug("Running story "+ storyPath);
-            StoryLoader loader = getStoryLoader();
-            Story story = loader.loadStory(storyPath, storyPackage);
-			story.specify();
-            storyRunner.run(story);
+            for ( Iterator i = storyPaths.iterator(); i.hasNext(); ){
+                String storyPath = (String)i.next();
+                runStory(storyPackage, storyPath);
+            }
         } catch (Exception e) {
-            throw new MojoExecutionException("Failed to run story "+storyPath+" with package "+storyPackage, e);
+            throw new MojoExecutionException("Failed to run stories "+storyPaths+" with package "+storyPackage, e);
         }
     }
 
-  
+    private void runStory(String storyPackage, String storyPath) throws MalformedURLException {
+        getLog().debug("Running story "+ storyPath+" using package "+storyPackage);
+        StoryLoader loader = getStoryLoader();
+        Story story = loader.loadStory(storyPath, storyPackage);
+        story.specify();
+        storyRunner.run(story);
+    }
+
 }
