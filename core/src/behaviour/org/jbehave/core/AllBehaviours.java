49b469af20fd4888e1e57d006e852fc6704f2c01
wolfdancer <wolfdancer@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-09-07 05:19:51 +0000

    changed the AllBehaviours in core to use the scanner from cotta

--- core/src/behaviour/org/jbehave/core/AllBehaviours.java
+++ core/src/behaviour/org/jbehave/core/AllBehaviours.java
@@ -7,32 +7,11 @@
  */
 package org.jbehave.core;
 
-import org.jbehave.core.behaviour.BehaviourBehaviours;
+import net.sf.cotta.jbehave.BehavioursLoader;
 import org.jbehave.core.behaviour.Behaviours;
-import org.jbehave.core.exception.ExceptionBehaviours;
-import org.jbehave.core.listener.ListenerBehaviours;
-import org.jbehave.core.matchers.MatchersBehaviours;
-import org.jbehave.core.minimock.MiniMockBehaviours;
-import org.jbehave.core.mock.MockBehaviours;
-import org.jbehave.core.result.ResultBehaviours;
-import org.jbehave.core.story.StoryBehaviours;
-import org.jbehave.core.threaded.ThreadedBehaviours;
-import org.jbehave.core.util.UtilBehaviours;
 
 public class AllBehaviours implements Behaviours {
     public Class[] getBehaviours() {
-        return new Class[] {
-                BehaviourRunnerBehaviour.class,
-                BehaviourBehaviours.class,
-                ExceptionBehaviours.class,
-                ListenerBehaviours.class,
-                MatchersBehaviours.class,
-                MiniMockBehaviours.class,
-                MockBehaviours.class,
-                ResultBehaviours.class,
-                StoryBehaviours.class,
-                ThreadedBehaviours.class,
-                UtilBehaviours.class
-        };
+        return new BehavioursLoader(getClass()).loadBehaviours();
     }
 }
