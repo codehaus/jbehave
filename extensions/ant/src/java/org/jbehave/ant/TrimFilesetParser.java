084678b3d60305d7c496a276fa19d50b501a4fcc
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2007-10-14 15:02:00 +0000

    Refactored TrimFilesetParser to avoid duplication. Added target in top-level build.xml to generate code for example stories.

--- extensions/ant/src/java/org/jbehave/ant/TrimFilesetParser.java
+++ extensions/ant/src/java/org/jbehave/ant/TrimFilesetParser.java
@@ -11,51 +11,38 @@ import org.apache.tools.ant.types.FileSet;
 public class TrimFilesetParser implements FilesetParser {
 
     public String[] getClassNames(FileSet fileset, Project project) {
-        DirectoryScanner ds = fileset.getDirectoryScanner(project);
-        String[] includedFiles = ds.getIncludedFiles();
+        String[] relativePaths = getRelativePaths(fileset, project);
         List classnames = new ArrayList();
-        for (int i = 0; i < includedFiles.length; i++) {
-            String filename = includedFiles[i].replace('\\', '/');
-
-            File base = ds.getBasedir();
-            File found = new File(base, filename);
-
-            String classname = found.getAbsolutePath().substring(
-                    base.getAbsolutePath().length() + 1).replace('\\', '/')
-                    .replace('/', '.');
-
-            classname = stripJavaOrClassFromEndOfFile(classname);
-            classnames.add(classname);
+        for (int i = 0; i < relativePaths.length; i++) {
+            classnames.add(stripFileExtension(relativePaths[i].replace('/', '.')));
         }
         return (String[]) classnames.toArray(new String[classnames.size()]);
     }
 
-	private String stripJavaOrClassFromEndOfFile(String classname) {
-		if (classname.length() > 6 && classname.substring(classname.length() - 6, classname.length()).equals(".class")) {
-		    classname = classname.substring(0, classname.length() - 6);
-		} else if (classname.length() > 5 && classname.substring(classname.length() - 5, classname.length()).equals(".java")) {
-		    classname = classname.substring(0, classname.length() -5);
-		}
-		return classname;
-	}
-	
     public String[] getRelativePaths(FileSet fileset, Project project) {
         DirectoryScanner ds = fileset.getDirectoryScanner(project);
         String[] includedFiles = ds.getIncludedFiles();
+        File base = ds.getBasedir();
         List paths = new ArrayList();
         for (int i = 0; i < includedFiles.length; i++) {
-            String filename = includedFiles[i].replace('\\', '/');
-
-            File base = ds.getBasedir();
-            File found = new File(base, filename);
-
-            String relativePath = found.getAbsolutePath().substring(
-                    base.getAbsolutePath().length() + 1).replace('\\', '/');
-            
-            paths.add(relativePath);
-        }        
+            paths.add(findRelativePath(base, includedFiles[i]));
+        }
         return (String[]) paths.toArray(new String[paths.size()]);
     }
 
+    private String findRelativePath(File base, String filePath) {
+        File found = new File(base, filePath.replace('\\', '/'));
+        return found.getAbsolutePath().substring(base.getAbsolutePath().length() + 1).replace('\\', '/');
+    }
+
+    private String stripFileExtension(String classname) {
+        if (classname.length() > 6 && classname.substring(classname.length() - 6, classname.length()).equals(".class")) {
+            classname = classname.substring(0, classname.length() - 6);
+        } else if (classname.length() > 5
+                && classname.substring(classname.length() - 5, classname.length()).equals(".java")) {
+            classname = classname.substring(0, classname.length() - 5);
+        }
+        return classname;
+    }
 
 }
