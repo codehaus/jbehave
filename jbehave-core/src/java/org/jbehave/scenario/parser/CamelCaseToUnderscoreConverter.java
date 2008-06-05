52783af33f6842af6cb08716658ace1a097e35dd
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-06-05 10:58:20 +0000

    Minor refactor to allow better readability.

--- jbehave-core/src/java/org/jbehave/scenario/parser/CamelCaseToUnderscoreConverter.java
+++ jbehave-core/src/java/org/jbehave/scenario/parser/CamelCaseToUnderscoreConverter.java
@@ -5,22 +5,23 @@ import java.util.regex.Pattern;
 
 public class CamelCaseToUnderscoreConverter implements ClassToFilenameConverter {
 
-	public String convert(Class<?> scenarioClass) {
+	private static final String UNDERSCORE = "_";
 
-		String packaje = scenarioClass.getPackage().getName();
-		String packajedir = packaje.replaceAll("\\.", "/");
+    public String convert(Class<?> scenarioClass) {
+
+		String packageDir = scenarioClass.getPackage().getName().replaceAll("\\.", "/");
 		
 		Matcher matcher = Pattern.compile("([A-Z].*?)([A-Z]|\\z)").matcher(scenarioClass.getSimpleName());
 		int startAt = 0;
 		StringBuilder builder = new StringBuilder();
 		while(matcher.find(startAt)) {
 			builder.append(matcher.group(1).toLowerCase());
-			builder.append("_");
+			builder.append(UNDERSCORE);
 			startAt = matcher.start(2);
 		}
 		
-		String underscored = builder.substring(0, builder.length() - 1);
-		return packajedir + "/" + underscored;
+		String underscoredName = builder.substring(0, builder.length() - 1);
+		return packageDir + "/" + underscoredName;
 	}
 
 }
