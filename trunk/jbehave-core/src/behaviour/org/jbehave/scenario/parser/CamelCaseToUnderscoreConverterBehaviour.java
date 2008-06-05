9840f2afc92c07ef8d22b1b30af1664b0a343bc2
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-06-05 10:58:20 +0000

    Minor refactor to allow better readability.

--- trunk/jbehave-core/src/behaviour/org/jbehave/scenario/parser/CamelCaseToUnderscoreConverterBehaviour.java
+++ trunk/jbehave-core/src/behaviour/org/jbehave/scenario/parser/CamelCaseToUnderscoreConverterBehaviour.java
@@ -1,17 +1,17 @@
 package org.jbehave.scenario.parser;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jbehave.Ensure.ensureThat;
 
-import org.hamcrest.CoreMatchers;
-import org.jbehave.scenario.parser.CamelCaseToUnderscoreConverter;
 import org.junit.Test;
 
 public class CamelCaseToUnderscoreConverterBehaviour {
 
-	@Test
-	public void shouldConvertCamelCasedScenarioNameToUnderscore() {
-		CamelCaseToUnderscoreConverter converter = new CamelCaseToUnderscoreConverter();
-		ensureThat(converter.convert(CamelCaseToUnderscoreConverterBehaviour.class), CoreMatchers.equalTo("org/jbehave/scenario/parser/camel_case_to_underscore_converter_behaviour"));
-		
-	}
+    @Test
+    public void shouldConvertCamelCasedClassNameToUnderscoredName() {
+        CamelCaseToUnderscoreConverter converter = new CamelCaseToUnderscoreConverter();
+        ensureThat(converter.convert(CamelCaseToUnderscoreConverterBehaviour.class),
+                equalTo("org/jbehave/scenario/parser/camel_case_to_underscore_converter_behaviour"));
+
+    }
 }
