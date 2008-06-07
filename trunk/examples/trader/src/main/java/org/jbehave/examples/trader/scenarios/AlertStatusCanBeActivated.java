9f51789624369bfde35015f9f6ab98700ec8cfd8
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-06-07 08:47:56 +0000

    Added default constructor to scenario so it can be run as unit test.

--- trunk/examples/trader/src/main/java/org/jbehave/examples/trader/scenarios/AlertStatusCanBeActivated.java
+++ trunk/examples/trader/src/main/java/org/jbehave/examples/trader/scenarios/AlertStatusCanBeActivated.java
@@ -5,6 +5,10 @@ import org.jbehave.scenario.parser.ScenarioFileLoader;
 
 public class AlertStatusCanBeActivated extends Scenario {
 
+    public AlertStatusCanBeActivated() {
+        this(Thread.currentThread().getContextClassLoader());
+    }
+
     public AlertStatusCanBeActivated(ClassLoader classLoader) {
         super(new ScenarioFileLoader(classLoader), new StockSteps());
     }
