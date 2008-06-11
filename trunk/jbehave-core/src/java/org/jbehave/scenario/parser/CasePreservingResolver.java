2ce5281930117c7ef32a2651065e6962430bdeb3
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-06-11 18:39:18 +0000

    Added case preserving scenario filename resolver, so that MyScenario.java can be resolved to MyScenario.scenario

---
+++ trunk/jbehave-core/src/java/org/jbehave/scenario/parser/CasePreservingResolver.java
@@ -0,0 +1,18 @@
+package org.jbehave.scenario.parser;
+
+import org.jbehave.scenario.Scenario;
+
+public class CasePreservingResolver implements ScenarioFileNameResolver {
+
+	private final String extension;
+
+    public CasePreservingResolver(String extension) {
+        this.extension = extension;
+    }
+
+    public String resolve(Class<? extends Scenario> scenarioClass) {
+		String packageDir = scenarioClass.getPackage().getName().replaceAll("\\.", "/");		
+		return packageDir + "/" + scenarioClass.getSimpleName() + extension;
+	}
+
+}
