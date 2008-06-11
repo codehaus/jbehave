cfbce117629889f1b9b45b21701e88fa7d722605
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-06-11 15:43:22 +0000

    Added behaviour for scenario with defaults.

---
+++ jbehave-core/src/behaviour/org/jbehave/scenario/MyDefaultScenario.java
@@ -0,0 +1,11 @@
+package org.jbehave.scenario;
+
+import org.jbehave.scenario.steps.Steps;
+
+public class MyDefaultScenario extends Scenario {
+
+    public MyDefaultScenario(Steps steps) {
+        super(steps);
+    }
+
+}
