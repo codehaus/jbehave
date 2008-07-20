cbeb12949b2125d4dab9dd2a9bebcfed7ffb8958
sirenian <sirenian@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-07-20 14:17:53 +0000

    [Liz] Build is now working without maven (sorry, Mauro!) and running all behaviours and scenarios correctly. Also added functionality to replace newlines in args with system newlines (so it will work on any system).

--- jbehave-core/src/behaviour/org/jbehave/scenario/parser/scenarios/MyPendingScenario.java
+++ jbehave-core/src/behaviour/org/jbehave/scenario/parser/scenarios/MyPendingScenario.java
@@ -1,7 +1,16 @@
 package org.jbehave.scenario.parser.scenarios;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.jbehave.scenario.Scenario;
+import org.jbehave.scenario.parser.ScenarioFileLoader;
+import org.jbehave.scenario.reporters.PrintStreamScenarioReporter;
 
 public class MyPendingScenario extends Scenario {
 
+	public MyPendingScenario() {
+		// Making sure this doesn't output to the build while it's running
+		super(new ScenarioFileLoader(), new PrintStreamScenarioReporter(new PrintStream(new ByteArrayOutputStream())));
+	}
 }
