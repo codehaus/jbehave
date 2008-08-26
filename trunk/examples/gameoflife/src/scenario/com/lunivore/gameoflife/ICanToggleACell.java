ee001bd023d3fb25a66e4691da71955fa496398a
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-08-26 07:59:25 +0000

    Enhanced trader example to use list-based step arguments and the print stream monitor. Made gameoflife scenario runnable by maven build.

--- trunk/examples/gameoflife/src/scenario/com/lunivore/gameoflife/ICanToggleACell.java
+++ trunk/examples/gameoflife/src/scenario/com/lunivore/gameoflife/ICanToggleACell.java
@@ -1,13 +1,26 @@
 package com.lunivore.gameoflife;
 
+import org.jbehave.scenario.PropertyBasedConfiguration;
 import org.jbehave.scenario.Scenario;
+import org.jbehave.scenario.parser.PatternScenarioParser;
+import org.jbehave.scenario.parser.ScenarioFileLoader;
+import org.jbehave.scenario.parser.UnderscoredCamelCaseResolver;
 
 import com.lunivore.gameoflife.steps.GridSteps;
 
 public class ICanToggleACell extends Scenario {
-    
-    @SuppressWarnings("unchecked")
+
     public ICanToggleACell() {
         super(new GridSteps());
+    }
+
+    public ICanToggleACell(final ClassLoader classLoader) {
+        super(new PropertyBasedConfiguration() {
+            @Override
+            public ScenarioFileLoader forDefiningScenarios() {
+                return new ScenarioFileLoader(new UnderscoredCamelCaseResolver(), classLoader,
+                        new PatternScenarioParser());
+            }
+        }, new GridSteps());
     }
 }
--- trunk/examples/trader/src/main/java/org/jbehave/examples/trader/scenarios/StockSteps.java
+++ trunk/examples/trader/src/main/java/org/jbehave/examples/trader/scenarios/StockSteps.java
@@ -1,34 +1,38 @@
 package org.jbehave.examples.trader.scenarios;
 
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jbehave.Ensure.ensureThat;
+
+import java.util.List;
 
 import org.jbehave.examples.trader.model.Stock;
 import org.jbehave.scenario.annotations.Given;
 import org.jbehave.scenario.annotations.Then;
 import org.jbehave.scenario.annotations.When;
+import org.jbehave.scenario.parser.PrefixCapturingPatternBuilder;
+import org.jbehave.scenario.steps.PrintStreamStepMonitor;
 import org.jbehave.scenario.steps.Steps;
 
 public class StockSteps extends Steps {
-    
+
     private double threshold;
     private Stock stock;
-    
-    public StockSteps(double threshold){
+
+    public StockSteps(double threshold) {
+        super(new PrefixCapturingPatternBuilder(), new PrintStreamStepMonitor(), "Given", "When", "Then", "And");
         this.threshold = threshold;
     }
-    
-    @Given("a stock of price $price")
-    public void aStockOfPrice(double price) {
-        stock = new Stock(price, threshold);
+
+    @Given("a stock of prices $prices")
+    public void aStockOfPrice(List<Double> prices) {
+        stock = new Stock(prices, threshold);
     }
-    
+
     @When("the stock is traded at $price")
     public void theStockIsTradedAt(double price) {
         stock.tradeAt(price);
     }
-    
+
     @Then("the alert status should be $status")
     public void theAlertStatusShouldBe(String status) {
         ensureThat(stock.getStatus().name(), equalTo(status));
