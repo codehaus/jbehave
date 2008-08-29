c5d9803d6bc80bca8cfaccfa10c56fddbf6725f2
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-08-29 13:08:09 +0000

    Added noughtsandcrosses example to maven build.

--- examples/noughtsandcrosses/src/scenario/com/lunivore/noughtsandcrosses/PlayersCanHazTurns.java
+++ examples/noughtsandcrosses/src/scenario/com/lunivore/noughtsandcrosses/PlayersCanHazTurns.java
@@ -3,6 +3,9 @@ package com.lunivore.noughtsandcrosses;
 import org.jbehave.scenario.MostUsefulConfiguration;
 import org.jbehave.scenario.Scenario;
 import org.jbehave.scenario.definition.KeyWords;
+import org.jbehave.scenario.parser.PatternScenarioParser;
+import org.jbehave.scenario.parser.ScenarioFileLoader;
+import org.jbehave.scenario.parser.UnderscoredCamelCaseResolver;
 
 import com.lunivore.noughtsandcrosses.steps.LolCatzSteps;
 
@@ -19,4 +22,18 @@ public class PlayersCanHazTurns extends Scenario {
             }
         }, new LolCatzSteps());
     }
+
+    public PlayersCanHazTurns(final ClassLoader classLoader) {
+        super(new MostUsefulConfiguration() {
+            public KeyWords keywords() {
+                return new KeyWords("I can haz", "Gief", "Wen", "Den", "And");
+            }
+            public ScenarioFileLoader forDefiningScenarios() {
+                return new ScenarioFileLoader(new UnderscoredCamelCaseResolver(), classLoader,
+                        new PatternScenarioParser(this));
+            }
+        }, new LolCatzSteps());
+    }
+    
 }
+
