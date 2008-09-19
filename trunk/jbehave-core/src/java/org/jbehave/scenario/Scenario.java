3e25dab6ffab21f385ccbbd9ecf49e065101812c
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-09-19 13:06:25 +0000

    JBEHAVE-132: Refactored JUnitScenario to be decorator/adapter of an AbstractScenario, which is test-framework agnostic.

--- trunk/jbehave-core/src/java/org/jbehave/scenario/Scenario.java
+++ trunk/jbehave-core/src/java/org/jbehave/scenario/Scenario.java
@@ -1,29 +1,17 @@
 package org.jbehave.scenario;
 
-import org.jbehave.scenario.definition.KeyWords;
-import org.jbehave.scenario.parser.ScenarioNameResolver;
-
 /**
  * <p>
  * Scenario represents the main interface to run a scenario.
  * </p>
  * <p>
- * Extend the implementation that uses the test framework of your choice, eg
- * JUnitScenario, and call the class after your scenario, eg: "ICanLogin.java".
+ * Typically users will need to extend an abstract implementation, such as
+ * {@link AbstractScenario} or a decorator scenarios, such as
+ * {@link JUnitScenario}, which also provide support for test frameworks.
  * </p>
- * <p>
- * Your Scenario class should be in a matching text file in the same place, eg
- * "i_can_login". The scenario name used can be configured via the
- * {@link ScenarioNameResolver}.
- * </p>
- * <p>
- * Write some steps in your text scenario, starting each new step with Given,
- * When, Then or And. The keywords can be configured via the {@link KeyWords}
- * class, eg they can be translated/localized to other languages.
- * </p>
- * <p>
- * Then move on to extending the Steps class.
- * </p>
+ * 
+ * @see AbstractScenario
+ * @see JUnitScenario
  */
 public interface Scenario {
 
