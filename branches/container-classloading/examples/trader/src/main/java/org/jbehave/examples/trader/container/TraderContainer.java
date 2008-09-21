b88227fe44c77a10780be76052ade57cbc737282
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-09-21 22:54:41 +0000

    Moved container package to separate jar to prove that it solves the classloading issues in trader example.

--- branches/container-classloading/examples/trader/src/main/java/org/jbehave/examples/trader/container/TraderContainer.java
+++ branches/container-classloading/examples/trader/src/main/java/org/jbehave/examples/trader/container/TraderContainer.java
@@ -1,9 +1,13 @@
 package org.jbehave.examples.trader.container;
 
+import org.jbehave.container.pico.XMLPicoContainer;
+
 
 //FIXME there are classloading issues (only using maven plugin) if TraderContainer extends the XMLPicoContainer cointained in core
 //Not quite sure what the root cause is - need more investigation. 
-public class TraderContainer extends XMLContainer {
+//NOTE separating jbehave-core from jbehave-container jar fixes the problem!  Possible issue with classloader hierarchy?  Could d
+//it bite somewhere else too - eg in a webapp container?
+public class TraderContainer extends XMLPicoContainer {
 
     public TraderContainer(ClassLoader classLoader) {
         super("org/jbehave/examples/trader/container/trader.xml", classLoader);
