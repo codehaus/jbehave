136329a0cc9f0faa942feffa6143a6e51cd2e0e8
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-09-29 07:45:17 +0000

    JBEHAVE-134:  Pulled up method to return candidate steps for an injected class, which may not be the implementing class.

--- jbehave-core/src/java/org/jbehave/scenario/steps/CandidateSteps.java
+++ jbehave-core/src/java/org/jbehave/scenario/steps/CandidateSteps.java
@@ -1,10 +1,22 @@
 package org.jbehave.scenario.steps;
 
 /**
- * Represents the set of candidate steps that can be performed
+ * Represents the list of candidate steps that can be performed
  */
 public interface CandidateSteps {
 
+    /**
+     * Return all the steps that can be performed by the implementing class
+     * 
+     * @return The list of candidate steps
+     */
     CandidateStep[] getSteps();
+
+    /**
+     * Return all the steps that can be performed by the given class
+     * 
+     * @return The list of candidate steps
+     */
+    CandidateStep[] getSteps(Class<?> stepsClass);
 
 }
