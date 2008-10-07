6c8a695c3e6f56791ef465471c2fa3e047efb244
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-10-07 20:51:37 +0000

    Updated examples to latest snapshot version.

--- examples/noughtsandcrosses/install-file.sh
+++ examples/noughtsandcrosses/install-file.sh
@@ -12,4 +12,4 @@ GROUP_ID=$1
 ARTIFACT_ID=$2
 VERSION=$3
 FILE=$4
-$MVN_GOAL -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dfile=$FILE -Dpackaging=jar -DgeneratePom=true 
+echo $MVN_GOAL -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dfile=$FILE -Dpackaging=jar -DgeneratePom=true 
