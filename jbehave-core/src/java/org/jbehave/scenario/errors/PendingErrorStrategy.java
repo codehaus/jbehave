97b65b4bb92c792b76c99432a313fa46efa547e4
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2008-11-06 08:53:53 +0000

    JBEHAVE-147:  Added javadocs.  Simplified exception message - which was potentially ambiguous.

--- jbehave-core/src/java/org/jbehave/scenario/errors/PendingErrorStrategy.java
+++ jbehave-core/src/java/org/jbehave/scenario/errors/PendingErrorStrategy.java
@@ -1,15 +1,29 @@
 package org.jbehave.scenario.errors;
 
-
+/**
+ * PendingErrorStrategy allows to define how pending error are handled. Two
+ * standard strategies are provided:
+ * <ul>
+ * <li>{@link PASSING}: passes scenarios upon pending errors</li>
+ * <li>{@link FAILING}: fails scenario upon pending errors</li>
+ * </ul>
+ */
 public interface PendingErrorStrategy extends ErrorStrategy {
 
-    PendingErrorStrategy PASSING = new PendingErrorStrategy() { 
-        public void handleError(Throwable throwable) {}
-    };
-    
-    PendingErrorStrategy FAILING = new PendingErrorStrategy() {
-        public void handleError(Throwable throwable) throws Throwable {
-            throw throwable;
-        } 
-    };
+	/**
+	 * Strategy that passes scenarios upon pending errors
+	 */
+	PendingErrorStrategy PASSING = new PendingErrorStrategy() {
+		public void handleError(Throwable throwable) {
+		}
+	};
+
+	/**
+	 * Strategy that fails scenario upon pending errors
+	 */
+	PendingErrorStrategy FAILING = new PendingErrorStrategy() {
+		public void handleError(Throwable throwable) throws Throwable {
+			throw throwable;
+		}
+	};
 }
