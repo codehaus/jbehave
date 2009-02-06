7759ccabf571e51dc9ce7468be27d71c8c9524f7
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-02-06 12:27:15 +0000

    Made selenium dependencies protected so they can be accessed by subclasses. Added javadocs.

--- sandbox/jbehave-web/web-selenium/src/main/java/org/jbehave/web/selenium/SeleniumSteps.java
+++ sandbox/jbehave-web/web-selenium/src/main/java/org/jbehave/web/selenium/SeleniumSteps.java
@@ -8,14 +8,22 @@ import org.jbehave.scenario.steps.Steps;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
-import com.thoughtworks.selenium.condition.Condition;
 import com.thoughtworks.selenium.condition.ConditionRunner;
 import com.thoughtworks.selenium.condition.JUnitConditionRunner;
 
+/**
+ * Steps implementation that can be used in Selenium-based scenarios. It
+ * provides annotated methods to start and stop Selenium before and after
+ * scenarios. It also provides defaults for Selenium and ConditionRunner
+ * dependencies, which may be overridden by user when providing the
+ * implementation of scenario steps.
+ * 
+ * @author Mauro Talevi
+ */
 public class SeleniumSteps extends Steps {
 
-	private final Selenium selenium;
-	private final ConditionRunner runner;
+	protected final Selenium selenium;
+	protected final ConditionRunner runner;
 
 	public SeleniumSteps() {
 		this.selenium = createSelenium();
@@ -33,20 +41,39 @@ public class SeleniumSteps extends Steps {
 		selenium.stop();
 	}
 
+	/**
+	 * Creates Selenium used by the Steps, by default {@link DefaultSelenium}
+	 * using "*firefox" as browser on localhost.
+	 * 
+	 * Users may override this method to provide their own custom instance of
+	 * Selenium.
+	 * 
+	 * @return A Selenium instance
+	 */
 	protected Selenium createSelenium() {
 		return new DefaultSelenium("localhost", 4444, "*firefox",
 				"http://localhost:8080");
 	}
 
-	protected JUnitConditionRunner createConditionRunner(Selenium selenium) {
+	/**
+	 * Creates ConditionRunner used by the Steps, by default
+	 * {@link JUnitConditionRunner}.
+	 * 
+	 * Users may override this method to provide their own custom instance of
+	 * ConditionRunner.
+	 * 
+	 * @param selenium the Selenium instance
+	 * @return A ConditionRunner
+	 */
+	protected ConditionRunner createConditionRunner(Selenium selenium) {
 		return new JUnitConditionRunner(selenium, 10, 100, 1000);
 	}
 
-	protected void waitFor(Condition condition) {
-		runner.waitFor(condition);
-		waitFor(2);
-	}
-
+	/**
+	 * Waits for a number of seconds
+	 * 
+	 * @param seconds the number of seconds to sleep
+	 */
 	protected void waitFor(int seconds) {
 		try {
 			TimeUnit.SECONDS.sleep(seconds);
