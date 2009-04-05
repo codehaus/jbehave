1056c0a51fd2a8f7e5cedd40227d26a4eb9cdc51
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-04-05 10:22:24 +0000

    JBEHAVE-127:  Fixed regression with 2.1.x.  Now supports configurable resolution pattern, one treating numbers as upper case letters (default) and another treating them as lower case (the 2.1.x default).

--- core/jbehave-core/src/behaviour/org/jbehave/scenario/parser/UnderscoredCamelCaseResolverBehaviour.java
+++ core/jbehave-core/src/behaviour/org/jbehave/scenario/parser/UnderscoredCamelCaseResolverBehaviour.java
@@ -2,6 +2,7 @@ package org.jbehave.scenario.parser;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jbehave.Ensure.ensureThat;
+import static org.jbehave.scenario.parser.UnderscoredCamelCaseResolver.NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN;
 
 import org.jbehave.scenario.JUnitScenario;
 import org.junit.Test;
@@ -10,29 +11,36 @@ public class UnderscoredCamelCaseResolverBehaviour {
 
     @Test
     public void shouldResolveCamelCasedClassNameToUnderscoredName() {
-        UnderscoredCamelCaseResolver resolver = new UnderscoredCamelCaseResolver();
+    	ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver();
         ensureThat(resolver.resolve(CamelCaseScenario.class),
                 equalTo("org/jbehave/scenario/parser/camel_case_scenario"));
-
     }
-    
-    
+        
     @Test
     public void shouldResolveCamelCasedClassNameToUnderscoredNameWithExtension() {
-        UnderscoredCamelCaseResolver resolver = new UnderscoredCamelCaseResolver(".scenario");
+    	ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver(".scenario");
         ensureThat(resolver.resolve(CamelCase.class),
                 equalTo("org/jbehave/scenario/parser/camel_case.scenario"));
-
     }
     
     @Test
-    public void shouldResolveCamelCasedClassNameWithNumbers() {
-        UnderscoredCamelCaseResolver resolver = new UnderscoredCamelCaseResolver();
-        ensureThat(resolver.resolve(CamelCaseWith3Dates.class),
-                equalTo("org/jbehave/scenario/parser/camel_case_with_3_dates"));
-
+    public void shouldResolveCamelCasedClassNameWithNumbersTreatedAsUpperCaseLetters() {
+    	ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver();
+        ensureThat(resolver.resolve(CamelCaseWithA3Qualifier.class),
+                equalTo("org/jbehave/scenario/parser/camel_case_with_a_3_qualifier"));
+        ensureThat(resolver.resolve(CamelCaseWithA33Qualifier.class),
+                equalTo("org/jbehave/scenario/parser/camel_case_with_a_3_3_qualifier"));
     }
     
+    @Test
+    public void shouldResolveCamelCasedClassNameWithNumbersTreatedAsLowerCaseLetters() {
+    	ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver("", NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN);
+        ensureThat(resolver.resolve(CamelCaseWithA3Qualifier.class),
+                equalTo("org/jbehave/scenario/parser/camel_case_with_a3_qualifier"));
+        ensureThat(resolver.resolve(CamelCaseWithA33Qualifier.class),
+                equalTo("org/jbehave/scenario/parser/camel_case_with_a33_qualifier"));
+    }
+        
     static class CamelCaseScenario extends JUnitScenario {
         
     }
@@ -41,7 +49,11 @@ public class UnderscoredCamelCaseResolverBehaviour {
         
     }
     
-    static class CamelCaseWith3Dates extends JUnitScenario {
+    static class CamelCaseWithA3Qualifier extends JUnitScenario {
+        
+    }
+    
+    static class CamelCaseWithA33Qualifier extends JUnitScenario {
         
     }
 }
