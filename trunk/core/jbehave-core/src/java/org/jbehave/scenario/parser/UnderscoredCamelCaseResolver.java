a071ad075ed4146765cb540d3a1f5433c8e77729
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-04-05 10:48:12 +0000

    JBEHAVE-127:  Switched to treat by default numbers as lower case letters, to ensure compatibility with 2.1.x.

--- trunk/core/jbehave-core/src/java/org/jbehave/scenario/parser/UnderscoredCamelCaseResolver.java
+++ trunk/core/jbehave-core/src/java/org/jbehave/scenario/parser/UnderscoredCamelCaseResolver.java
@@ -18,21 +18,21 @@ import org.jbehave.scenario.RunnableScenario;
  * "org/jbehave/scenario/i_can_login.scenario".
  * </p>
  * <p>
- * The default resolution pattern {@link NUMBERS_AS_UPPER_CASE_LETTERS_PATTERN}
- * treats numbers as upper case letters, eg:
+ * The default resolution pattern {@link NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN}
+ * treats numbers as lower case letters, eg:
  * "org.jbehave.scenario.ICanLoginTo1Site.java" ->
- * "org/jbehave/scenario/i_can_login_to_1_site"
+ * "org/jbehave/scenario/i_can_login_to1_site"
  * </p>
  * <p>
- * Choose {@link NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN} to treat numbers as
- * lower case letters, eg: "org.jbehave.scenario.ICanLoginTo1Site.java" ->
- * "org/jbehave/scenario/i_can_login_to1_site"
+ * Choose {@link NUMBERS_AS_UPPER_CASE_LETTERS_PATTERN} to treat numbers as
+ * uper case letters, eg: "org.jbehave.scenario.ICanLoginTo1Site.java" ->
+ * "org/jbehave/scenario/i_can_login_to_1_site"
  * </p>
  */
 public class UnderscoredCamelCaseResolver extends AbstractScenarioNameResolver {
 
-	public static final String NUMBERS_AS_UPPER_CASE_LETTERS_PATTERN = "([A-Z0-9].*?)([A-Z0-9]|\\z)";
 	public static final String NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN = "([A-Z].*?)([A-Z]|\\z)";
+	public static final String NUMBERS_AS_UPPER_CASE_LETTERS_PATTERN = "([A-Z0-9].*?)([A-Z0-9]|\\z)";
 	private static final String UNDERSCORE = "_";
 	private final String resolutionPattern;
 
@@ -41,7 +41,7 @@ public class UnderscoredCamelCaseResolver extends AbstractScenarioNameResolver {
 	}
 
 	public UnderscoredCamelCaseResolver(String extension) {
-		this(extension, NUMBERS_AS_UPPER_CASE_LETTERS_PATTERN);
+		this(extension, NUMBERS_AS_LOWER_CASE_LETTERS_PATTERN);
 	}
 
 	public UnderscoredCamelCaseResolver(String extension,
