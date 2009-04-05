4ac8ac8e308d8bc8aee2d822b03dd9cf5dd785ed
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-04-05 09:17:37 +0000

    Added test to verify that numbers are treated as lower-case letters.

--- branches/jbehave-2.1.x/jbehave-core/src/behaviour/org/jbehave/scenario/parser/UnderscoredCamelCaseResolverBehaviour.java
+++ branches/jbehave-2.1.x/jbehave-core/src/behaviour/org/jbehave/scenario/parser/UnderscoredCamelCaseResolverBehaviour.java
@@ -8,27 +8,40 @@ import org.junit.Test;
 
 public class UnderscoredCamelCaseResolverBehaviour {
 
-    @Test
-    public void shouldResolveCamelCasedClassNameToUnderscoredName() {
-        UnderscoredCamelCaseResolver resolver = new UnderscoredCamelCaseResolver();
-        ensureThat(resolver.resolve(CamelCaseScenario.class),
-                equalTo("org/jbehave/scenario/parser/camel_case_scenario"));
+	@Test
+	public void shouldResolveCamelCasedClassNameToUnderscoredName() {
+		ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver();
+		ensureThat(resolver.resolve(CamelCaseScenario.class),
+				equalTo("org/jbehave/scenario/parser/camel_case_scenario"));
 
-    }
-    
-    @Test
-    public void shouldResolveCamelCasedClassNameToUnderscoredNameWithExtension() {
-        UnderscoredCamelCaseResolver resolver = new UnderscoredCamelCaseResolver(".scenario");
-        ensureThat(resolver.resolve(CamelCase.class),
-                equalTo("org/jbehave/scenario/parser/camel_case.scenario"));
+	}
 
-    }
-    
-    static class CamelCaseScenario extends JUnitScenario {
-        
-    }
+	@Test
+	public void shouldResolveCamelCasedClassNameToUnderscoredNameWithExtension() {
+		ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver(
+				".scenario");
+		ensureThat(resolver.resolve(CamelCase.class),
+				equalTo("org/jbehave/scenario/parser/camel_case.scenario"));
 
-    static class CamelCase extends JUnitScenario {
-        
-    }
+	}
+
+	@Test
+	public void shouldResolveCamelCasedClassNameWithNumbersTreatedAsLowerCaseLetters() {
+		ScenarioNameResolver resolver = new UnderscoredCamelCaseResolver();
+		ensureThat(resolver.resolve(CamelCaseWithA333Qualifier.class),
+				equalTo("org/jbehave/scenario/parser/camel_case_with_a333_qualifier"));
+
+	}
+
+	static class CamelCaseScenario extends JUnitScenario {
+
+	}
+
+	static class CamelCase extends JUnitScenario {
+
+	}
+
+	static class CamelCaseWithA333Qualifier extends JUnitScenario {
+
+	}
 }
