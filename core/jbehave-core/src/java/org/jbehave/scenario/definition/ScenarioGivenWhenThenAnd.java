ac39929cae09aaf507b8e8b47213eaf901df004c
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-04-25 10:16:08 +0000

    [maven-release-plugin]  copy for tag jbehave-web-2.0-beta-5

--- core/jbehave-core/src/java/org/jbehave/scenario/definition/ScenarioGivenWhenThenAnd.java
+++
@@ -1,9 +0,0 @@
-package org.jbehave.scenario.definition;
-
-public class ScenarioGivenWhenThenAnd extends KeyWords {
-
-    public ScenarioGivenWhenThenAnd() {
-        super("Scenario:", "Given", "When", "Then", "And");
-    }
-
-}
