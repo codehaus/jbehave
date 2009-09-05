872b9fa8ca13818ef1c64a436db5bec52dcaa1f9
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-09-05 10:05:20 +0000

    JBEHAVE-173:  Added fine-grained monitoring of step arg lookup.

--- core/jbehave-core/src/java/org/jbehave/scenario/steps/StepMonitor.java
+++ core/jbehave-core/src/java/org/jbehave/scenario/steps/StepMonitor.java
@@ -14,4 +14,16 @@ public interface StepMonitor {
     void convertedValueOfType(String value, Type type, Object converted, Class<?> converterClass);
 
     void performing(String step);
+
+	void usingAnnotatedName(String name, int index);
+
+	void usingParameterName(String name, int index);
+
+	void usingTableAnnotatedName(String name, int index);
+
+	void usingTableParameterName(String name, int index);
+
+	void usingNaturalOrder(int index);
+
+	void foundArg(String arg, int index);
 }
