6938c3fbc7a898b34c9b44882a663fd8be5c6d57
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-09-07 22:26:12 +0000

    JBEHAVE-178: Allow output format message patterns to be overridden in PrintStreamScenarioReporter

--- trunk/core/jbehave-core/src/java/org/jbehave/scenario/reporters/PrintStreamScenarioReporter.java
+++ trunk/core/jbehave-core/src/java/org/jbehave/scenario/reporters/PrintStreamScenarioReporter.java
@@ -1,6 +1,7 @@
 package org.jbehave.scenario.reporters;
 
 import java.io.PrintStream;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -32,20 +33,20 @@ public class PrintStreamScenarioReporter implements ScenarioReporter {
 	}
 
 	public void successful(String step) {
-		output.println(step);
+		output.println(format("successful", "{0}", step));
 	}
 
 	public void pending(String step) {
-		output.println(step + " (PENDING)");
+		output.println(format("pending", "{0} (PENDING)", step));
 	}
 
 	public void notPerformed(String step) {
-		output.println(step + " (NOT PERFORMED)");
+		output.println(format("notPerformed", "{0} (NOT PERFORMED)", step));
 	}
 
 	public void failed(String step, Throwable cause) {
 		this.cause = cause;
-		output.println(step + " (FAILED)");
+		output.println(format("failed", "{0} (FAILED)", step));
 	}
 
 	public void afterScenario() {
@@ -58,33 +59,45 @@ public class PrintStreamScenarioReporter implements ScenarioReporter {
 
 	public void beforeScenario(String title) {
 		cause = null;
-		output.println("Scenario: " + title);
-		output.println();
+		output.println(format("beforeScenario", "Scenario: {0}\n", title));
 	}
 
 	public void afterStory() {
-
+		output.println(format("afterStory", "" ));
 	}
 
 	public void beforeStory(Blurb blurb) {
-		output.println(blurb.asString());
-		output.println();
+		output.println(format("beforeStory", "{0}", blurb.asString()));
 	}
 
 	public void givenScenarios(List<String> givenScenarios) {
-		output.println("GivenScenarios: " + givenScenarios);
+		output.println(format("givenScenarios", "GivenScenarios: {0}\n",
+				givenScenarios));
 	}
 
 	public void usingExamplesTable(ExamplesTable table) {
-		output.println("Using examples table:");
-		output.println();
-		output.println(table);
-		output.println();
+		output.println(format("usingExamplesTable",
+				"Using examples table:\n\n{0}\n\n", table));
 	}
 
 	public void usingTableRow(Map<String, String> tableRow) {
-		output.println("Using table row: " + tableRow);
-		output.println();
+		output.println(format("usingTableRow", "Using table row: {0}\n", tableRow));
+	}
+
+	protected String format(String key, String defaultPattern, Object... args) {
+		return MessageFormat.format(patternFor(key, defaultPattern), args);
+	}
+
+	/**
+	 * Provide format patterns for the output by key, conventionally equal to the method name.
+	 * If no pattern is found for key or needs to be overridden, the default pattern should be returned. 
+	 * 
+	 * @param key the format pattern key
+	 * @param defaultPattern the default pattern if no pattern is 
+	 * @return The format patter for the given key
+	 */
+	protected String patternFor(String key, String defaultPattern) {
+		return defaultPattern;
 	}
 
 }
