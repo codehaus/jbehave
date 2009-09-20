f57ba8cbd548db48e7b695f4ec7804874245c309
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-09-20 10:11:24 +0000

    Updated header.

--- trunk/core/distribution/src/site/resources/js/shBrushBdd.js
+++ trunk/core/distribution/src/site/resources/js/shBrushBdd.js
@@ -1,38 +1,14 @@
 /**
- * SyntaxHighlighter
- * http://alexgorbatchev.com/
- *
- * SyntaxHighlighter is donationware. If you are using it, please donate.
- * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
- *
- * @version
- * 2.0.320 (May 03 2009)
+ * SyntaxHighlighter brush for BDD grammar.
  * 
- * @copyright
- * Copyright (C) 2004-2009 Alex Gorbatchev.
- *
- * @license
- * This file is part of SyntaxHighlighter.
- * 
- * SyntaxHighlighter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * SyntaxHighlighter is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with SyntaxHighlighter.  If not, see <http://www.gnu.org/copyleft/lesser.html>.
+ * Adapted from shBrushJava.
  */
 SyntaxHighlighter.brushes.Bdd = function()
 {
 	var keywords =	'Given When Then And Scenario GivenScenarios Examples ExamplesTable ExamplesTableRow Pending NotPerformed Failed';
 	
 	this.regexList = [
-		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		// java keyword
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		// bdd keyword
 		];
 
 	this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
