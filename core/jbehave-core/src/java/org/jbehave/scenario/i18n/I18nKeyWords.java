0f67848ea59f51ae5b201a0d33169dd4e7e0fc54
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-10-04 12:37:43 +0000

    Verification of negative cases in i18n keywords behaviour.

--- core/jbehave-core/src/java/org/jbehave/scenario/i18n/I18nKeyWords.java
+++ core/jbehave-core/src/java/org/jbehave/scenario/i18n/I18nKeyWords.java
@@ -8,54 +8,83 @@ import java.util.ResourceBundle;
 
 import org.jbehave.scenario.definition.KeyWords;
 
+/**
+ * Add i18n support to Keywords, allowing to read the keywords from resource bundles for a given locale.
+ */
 public class I18nKeyWords extends KeyWords {
 
+	private static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
+	private static final StringEncoder DEFAULT_STRING_ENCODER = new StringEncoder();
 	private static final String DEFAULT_BUNDLE_NAME = "org/jbehave/scenario/i18n/keywords";
+	private static final ClassLoader DEFAULT_CLASS_LOADER = Thread
+			.currentThread().getContextClassLoader();
 
 	public I18nKeyWords() {
-        this(Locale.ENGLISH, new StringEncoder(), DEFAULT_BUNDLE_NAME,  Thread.currentThread().getContextClassLoader());
-    }
+		this(DEFAULT_LOCALE, DEFAULT_STRING_ENCODER, DEFAULT_BUNDLE_NAME,
+				DEFAULT_CLASS_LOADER);
+	}
 
-    public I18nKeyWords(Locale locale) {
-    	this(locale, new StringEncoder(),  DEFAULT_BUNDLE_NAME, Thread.currentThread().getContextClassLoader());
-    }
+	public I18nKeyWords(Locale locale) {
+		this(locale, DEFAULT_STRING_ENCODER, DEFAULT_BUNDLE_NAME,
+				DEFAULT_CLASS_LOADER);
+	}
 
-    public I18nKeyWords(Locale locale,  StringEncoder encoder) {
-    	this(locale, encoder, DEFAULT_BUNDLE_NAME, Thread.currentThread().getContextClassLoader());
-    }
+	public I18nKeyWords(Locale locale, StringEncoder encoder) {
+		this(locale, encoder, DEFAULT_BUNDLE_NAME, DEFAULT_CLASS_LOADER);
+	}
 
-    public I18nKeyWords(Locale locale, StringEncoder encoder, String bundleName, ClassLoader classLoader) {
-    	super(keywords(bundleName, locale, encoder, classLoader), encoder);
-    }
+	public I18nKeyWords(Locale locale, StringEncoder encoder,
+			String bundleName, ClassLoader classLoader) {
+		super(keywords(bundleName, locale, encoder, classLoader), encoder);
+	}
 
 	private static Map<String, String> keywords(String bundleName,
 			Locale locale, StringEncoder encoder, ClassLoader classLoader) {
-		ResourceBundle bundle = lookupBunde(bundleName, locale, classLoader);
+		ResourceBundle bundle = lookupBunde(bundleName.trim(), locale,
+				classLoader);
 		Map<String, String> keywords = new HashMap<String, String>();
-		for ( String key : KEYWORDS ) {
-			keywords.put(key, keyword(bundle, key, encoder));			
+		for (String key : KEYWORDS) {
+			keywords.put(key, keyword(key, bundle, encoder));
 		}
 		return keywords;
 	}
 
-	private static String keyword(ResourceBundle bundle, String name, StringEncoder encoder) {
-		return encoder.encode(bundle.getString(name));
+	private static String keyword(String name, ResourceBundle bundle,
+			StringEncoder encoder) {
+		try {
+			return encoder.encode(bundle.getString(name));
+		} catch (MissingResourceException e) {
+			throw new KeywordNotFoundExcepion(name, bundle);
+		}
 	}
 
-    private static ResourceBundle lookupBunde(String bundleName, Locale locale, ClassLoader classLoader) {
-        try {
-            return ResourceBundle.getBundle(bundleName.trim(), locale, classLoader);
-        } catch (MissingResourceException e) {
-            throw new ResourceBundleNotFoundExcepion(bundleName, locale, classLoader, e);
-        }
-    }
-    
-	@SuppressWarnings("serial")
-	public static final class ResourceBundleNotFoundExcepion extends RuntimeException {
+	private static ResourceBundle lookupBunde(String bundleName, Locale locale,
+			ClassLoader classLoader) {
+		try {
+			return ResourceBundle.getBundle(bundleName, locale, classLoader);
+		} catch (MissingResourceException e) {
+			throw new ResourceBundleNotFoundExcepion(bundleName, locale,
+					classLoader, e);
+		}
+	}
 
-		public ResourceBundleNotFoundExcepion(String bundleName,
-				Locale locale, ClassLoader classLoader, MissingResourceException cause) {
-			super("Resource bundle "+bundleName+" not found for locale "+locale+" in classLoader "+classLoader, cause);
+	@SuppressWarnings("serial")
+	public static final class ResourceBundleNotFoundExcepion extends
+			RuntimeException {
+
+		public ResourceBundleNotFoundExcepion(String bundleName, Locale locale,
+				ClassLoader classLoader, MissingResourceException cause) {
+			super("Resource bundle " + bundleName + " not found for locale "
+					+ locale + " in classLoader " + classLoader, cause);
+		}
+
+	}
+
+	@SuppressWarnings("serial")
+	public static final class KeywordNotFoundExcepion extends RuntimeException {
+
+		public KeywordNotFoundExcepion(String name, ResourceBundle bundle) {
+			super("Keyword " + name + " not found in resource bundle " + bundle);
 		}
 
 	}
--- core/jbehave-core/src/java/org/jbehave/scenario/i18n/StringEncoder.java
+++ core/jbehave-core/src/java/org/jbehave/scenario/i18n/StringEncoder.java
@@ -5,17 +5,31 @@ package org.jbehave.scenario.i18n;
 
 import java.io.UnsupportedEncodingException;
 
+/**
+ * Supports encoding of strings using specified charsets for encoding (i.e. from
+ * String to byte[]) and decoding (i.e. from byte[] to String).
+ */
 public class StringEncoder {
 
-	public static final String ISO_8859_1 = "ISO-8859-1";
 	public static final String UTF_8 = "UTF-8";
 	private String encoding;
 	private String decoding;
-	
+
+	/**
+	 * Creates an encoder using "UTF-8" for encoding and decoding
+	 */
 	public StringEncoder() {
 		this(UTF_8, UTF_8);
 	}
-	
+
+	/**
+	 * Creates an encoder using the specifed charsets for encoding and decoding
+	 * 
+	 * @param encoding
+	 *            the name of the encoding charset
+	 * @param decoding
+	 *            the name of the decoding charset
+	 */
 	public StringEncoder(String encoding, String decoding) {
 		this.encoding = encoding;
 		this.decoding = decoding;
@@ -28,7 +42,7 @@ public class StringEncoder {
 			throw new InvalidEncodingExcepion(value, e);
 		}
 	}
-	
+
 	@SuppressWarnings("serial")
 	public static final class InvalidEncodingExcepion extends RuntimeException {
 
