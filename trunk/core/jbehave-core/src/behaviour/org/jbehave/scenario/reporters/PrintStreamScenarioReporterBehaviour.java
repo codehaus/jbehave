c0d0a97fc1b11dd18174676b984370d1b2188b5f
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-12-12 16:17:32 +0000

    JBEHAVE-161:  Refactored PrintStreamScenarioReporterBehaviour to report same narrative to both plain-text and html reporters.

--- trunk/core/jbehave-core/src/behaviour/org/jbehave/scenario/reporters/PrintStreamScenarioReporterBehaviour.java
+++ trunk/core/jbehave-core/src/behaviour/org/jbehave/scenario/reporters/PrintStreamScenarioReporterBehaviour.java
@@ -1,18 +1,26 @@
 package org.jbehave.scenario.reporters;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jbehave.Ensure.ensureThat;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.jbehave.scenario.Scenario;
+import org.jbehave.scenario.definition.Blurb;
+import org.jbehave.scenario.definition.ExamplesTable;
+import org.jbehave.scenario.definition.ScenarioDefinition;
+import org.jbehave.scenario.definition.StoryDefinition;
 import org.jbehave.scenario.i18n.I18nKeyWords;
 import org.junit.Test;
 
@@ -23,30 +31,103 @@ public class PrintStreamScenarioReporterBehaviour {
 
     @Test
     public void shouldReportEventsToPrintStream() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ScenarioReporter reporter = new PrintStreamScenarioReporter(new PrintStream(out));
+        // Given
+        OutputStream out = new ByteArrayOutputStream();
+        ScenarioReporter reporter = new PrintStreamScenarioReporter(new PrintStream(out));        
+        
+        // When 
+        narrateAnInterestingStory(reporter);
+
+        // Then
+        String expected = 
+        "An interesting story\n" +
+        "Scenario: I ask for a loan\n" +
+        "GivenScenarios: [/given/scenario1,/given/scenario2]\n" +
+        "Given I have a balance of $50\n" +
+        "When I request $20\n" +
+        "When I ask Liz for a loan of $100\n" +
+        "Then I should have a balance of $30 (PENDING)\n" +
+        "Then I should have $20 (NOT PERFORMED)\n" +
+        "Then I don't return loan (FAILED)\n" +
+        "Examples:\n\n" +
+        "|money|to|\n" +
+        "|$30|Mauro|\n" +
+        "|$50|Paul|\n" +
+        "\n\n" + // Examples table
+        "\nExample: {to=Mauro, money=$30}\n" +
+        "\nExample: {to=Paul, money=$50}\n" +
+        "\n\n";  // end of scenario and story        
+        ensureThatOutputIs(out, expected);
+    }
+    
+    @Test
+    public void shouldReportEventsToHtmlPrintStream() {
+        // Given
+        final OutputStream out = new ByteArrayOutputStream();
+        ScenarioReporter reporter = new HtmlPrintStreamScenarioReporter(new PrintStreamFactory(){
+
+            public PrintStream getPrintStream() {
+                return new PrintStream(out);
+            }});
+        
+        // When 
+        narrateAnInterestingStory(reporter);
+
+        // Then
+        String expected = 
+        "<div class=\"story\">\n<h1>An interesting story</h1>\n" +
+        "<div class=\"scenario\">\n<h2>Scenario: I ask for a loan</h2>\n" +
+        "<div class=\"givenScenarios\">GivenScenarios: [/given/scenario1,/given/scenario2]</div>\n" +
+        "<div class=\"step.successful\">Given I have a balance of $50</div>\n" +
+        "<div class=\"step.successful\">When I request $20</div>\n" +
+        "<div class=\"step.successful\">When I ask Liz for a loan of $100</div>\n" +
+        "<div class=\"step.pending\">Then I should have a balance of $30<span class=\"keyword.pending\">(PENDING)</span></div>\n" +
+        "<div class=\"step.notPerformed\">Then I should have $20<span class=\"keyword.notPerformed\">(NOT PERFORMED)</span></div>\n" +
+        "<div class=\"step.failed\">Then I don't return loan<span class=\"keyword.failed\">(FAILED)</span></div>\n" +
+        "<h3 class=\"examplesTable\">Examples:</h3>\n<table class=\"examplesTable\">\n<thead>\n<tr>\n<th>to</th><th>money</th></tr>\n</thead>\n<tbody>\n<tr>\n<td>Mauro</td><td>$30</td></tr>\n<tr>\n<td>Paul</td><td>$50</td></tr>\n</tbody>\n</table>\n"+
+        "\n<h3 class=\"examplesTableRow\">Example: {to=Mauro, money=$30}</h3>\n" +
+        "\n<h3 class=\"examplesTableRow\">Example: {to=Paul, money=$50}</h3>\n"+
+        "</div>\n</div>\n";  // end of scenario and story 
+        ensureThatOutputIs(out, expected);        
+    }    
+    
+    private void narrateAnInterestingStory(ScenarioReporter reporter) {
+        StoryDefinition story = new StoryDefinition(new Blurb("An interesting story"), new ArrayList<ScenarioDefinition>(), "/path/to/story");
+        boolean embeddedStory = true;
+        reporter.beforeStory(story, embeddedStory);
+        String title = "I ask for a loan";
+        reporter.beforeScenario(title);
+        reporter.givenScenarios(asList("/given/scenario1,/given/scenario2"));
         reporter.successful("Given I have a balance of $50");
         reporter.successful("When I request $20");
         reporter.successful("When I ask Liz for a loan of $100");
         reporter.pending("Then I should have a balance of $30");
         reporter.notPerformed("Then I should have $20");
-        
-        ensureThat(out.toString(), equalTo(
-                "Given I have a balance of $50" + NL +
-                "When I request $20" + NL +
-                "When I ask Liz for a loan of $100" + NL +
-                "Then I should have a balance of $30 (PENDING)" + NL +
-                "Then I should have $20 (NOT PERFORMED)" + NL));
+        reporter.failed("Then I don't return loan", new Exception("Naughty me!"));
+        ExamplesTable table = new ExamplesTable("|money|to|\n|$30|Mauro|\n|$50|Paul|\n");
+        reporter.examplesTable(table);
+        reporter.examplesTableRow(table.getRow(0));
+        reporter.examplesTableRow(table.getRow(1));
+        reporter.afterScenario();
+        reporter.afterStory(embeddedStory);
     }
-    
+
+    private void ensureThatOutputIs(OutputStream out, String expected) {
+        // JUnit assertion allows easier comparison of strings in IDE
+        assertEquals(expected, out.toString());
+        //ensureThat(out.toString(), equalTo(expected));
+    }
+
     @Test
     public void shouldReportThrowablesWhenToldToDoSo() {
+        // Given
         IllegalAccessException exception = new IllegalAccessException("Leave my money alone!");
-        ByteArrayOutputStream stackTrace = new ByteArrayOutputStream();
-        exception.printStackTrace(new PrintStream(stackTrace));
-        
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream stackTrace = new ByteArrayOutputStream();
+        exception.printStackTrace(new PrintStream(stackTrace));        
+        OutputStream out = new ByteArrayOutputStream();
         ScenarioReporter reporter = new PrintStreamScenarioReporter(new PrintStream(out), new Properties(), new I18nKeyWords(), true);
+        
+        // When
         reporter.beforeScenario("A title");
         reporter.successful("Given I have a balance of $50");
         reporter.successful("When I request $20");
@@ -54,18 +135,23 @@ public class PrintStreamScenarioReporterBehaviour {
         reporter.pending("Then I should have a balance of $30");
         reporter.notPerformed("Then I should have $20");
         reporter.afterScenario();
+
+        // Then
+        String expected = 
+        "Scenario: A title\n" +
+        "Given I have a balance of $50\n" +
+        "When I request $20\n" +
+        "When I ask Liz for a loan of $100 (FAILED)\n" +
+        "Then I should have a balance of $30 (PENDING)\n" +
+        "Then I should have $20 (NOT PERFORMED)\n" +
+        NL + stackTrace + NL;
+        ensureThatOutputIs(out, expected);
         
-        ensureThat(out.toString(), equalTo(
-                "Scenario: A title" + NL +
-                "Given I have a balance of $50" + NL +
-                "When I request $20" + NL +
-                "When I ask Liz for a loan of $100 (FAILED)" + NL +
-                "Then I should have a balance of $30 (PENDING)" + NL +
-                "Then I should have $20 (NOT PERFORMED)" + NL +
-                NL + stackTrace + NL));
-        
+        // Given
         out = new ByteArrayOutputStream();
         reporter = new PrintStreamScenarioReporter(new PrintStream(out));
+        
+        // When
         reporter.beforeScenario("A title");
         reporter.successful("Given I have a balance of $50");
         reporter.successful("When I request $20");
@@ -74,68 +160,89 @@ public class PrintStreamScenarioReporterBehaviour {
         reporter.notPerformed("Then I should have $20");
         reporter.afterScenario();
         
+        // Then
         ensureThat(!out.toString().contains(stackTrace.toString()));
     }
 
 
     @Test
     public void shouldReportEventsToPrintStreamWithCustomPatterns() {
+        // Given
         IllegalAccessException exception = new IllegalAccessException("Leave my money alone!");
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream out = new ByteArrayOutputStream();
         Properties patterns = new Properties();
         patterns.setProperty("pending", "{0} - {1} - need to implement me\n");
         patterns.setProperty("failed", "{0} <<< {1}\n");
         patterns.setProperty("notPerformed", "{0} : {1} (because of previous pending)\n");
-
 		ScenarioReporter reporter = new PrintStreamScenarioReporter(new PrintStream(out),  patterns, new I18nKeyWords(), true);
+		
+		// When
         reporter.successful("Given I have a balance of $50");
         reporter.successful("When I request $20");
         reporter.failed("When I ask Liz for a loan of $100", exception);
         reporter.pending("Then I should have a balance of $30");
         reporter.notPerformed("Then I should have $20");
         
-        ensureThat(out.toString(), equalTo(
-                "Given I have a balance of $50" + NL +
-                "When I request $20" + NL +
-                "When I ask Liz for a loan of $100 <<< FAILED" + NL +
-                "Then I should have a balance of $30 - PENDING - need to implement me" + NL +
-                "Then I should have $20 : NOT PERFORMED (because of previous pending)" + NL));
+        // Then
+        String expected = "Given I have a balance of $50\n" +
+        "When I request $20\n" +
+        "When I ask Liz for a loan of $100 <<< FAILED\n" +
+        "Then I should have a balance of $30 - PENDING - need to implement me\n" +
+        "Then I should have $20 : NOT PERFORMED (because of previous pending)\n";
+
+        ensureThatOutputIs(out, expected);
         
     }
 
     @Test
     public void shouldReportEventsToPrintStreamInItalian() {
+        // Given
         IllegalAccessException exception = new IllegalAccessException("Lasciate in pace i miei soldi!");
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream out = new ByteArrayOutputStream();
         I18nKeyWords keywords = new I18nKeyWords(Locale.ITALIAN);
 		ScenarioReporter reporter = new PrintStreamScenarioReporter(new PrintStream(out),  new Properties(), keywords, true);
+		
+		// When
         reporter.successful("Dato che ho un saldo di $50");
         reporter.successful("Quando richiedo $20");
         reporter.failed("Quando chiedo a Liz un prestito di $100", exception);
         reporter.pending("Allora dovrei avere un saldo di $30");
         reporter.notPerformed("Allora dovrei avere $20");
         
-        ensureThat(out.toString(), equalTo(
-                "Dato che ho un saldo di $50" + NL +
-                "Quando richiedo $20" + NL +
-                "Quando chiedo a Liz un prestito di $100 (FALLITO)" + NL +
-                "Allora dovrei avere un saldo di $30 (PENDENTE)" + NL +
-                "Allora dovrei avere $20 (NON ESEGUITO)" + NL));
+        // Then
+        String expected = 
+        "Dato che ho un saldo di $50\n" +
+        "Quando richiedo $20\n" +
+        "Quando chiedo a Liz un prestito di $100 (FALLITO)\n" +
+        "Allora dovrei avere un saldo di $30 (PENDENTE)\n" +
+        "Allora dovrei avere $20 (NON ESEGUITO)\n";
+        
+        ensureThatOutputIs(out, expected);
         
     }
 
     @Test
     public void shouldCreateAndWriteToFilePrintStreamForScenarioClass() throws IOException{
+        // Given
         Class<MyScenario> scenarioClass = MyScenario.class;
+        File file = fileFor(scenarioClass);
+        file.delete(); 
+        ensureThat(!file.exists());    
+        
+        // When
         FilePrintStreamFactory factory = new FilePrintStreamFactory(scenarioClass);
         PrintStream printStream = factory.getPrintStream();
         printStream.print("Hello World");
-        
-        File outputDirectory = FilePrintStreamFactory.outputDirectory(scenarioClass);
-        String fileName = FilePrintStreamFactory.fileName(scenarioClass, FilePrintStreamFactory.HTML);
-        File file = new File(outputDirectory, fileName);
+
+        // Then
         ensureThat(file.exists());    
         ensureThat(IOUtils.toString(new FileReader(file)), equalTo("Hello World"));
+    }
+
+    private File fileFor(Class<MyScenario> scenarioClass) {
+        File outputDirectory = FilePrintStreamFactory.outputDirectory(scenarioClass);
+        String fileName = FilePrintStreamFactory.fileName(scenarioClass, FilePrintStreamFactory.HTML);
+        return new File(outputDirectory, fileName);
     }
     
     private static class MyScenario extends Scenario {
