dbfd67acf03ba1a40d0f26223ef256d75b8c2c80
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-12-22 20:54:55 +0000

    JBEHAVE-165:  Added batch mode in running scenarios via Ant and Maven. Added non_successful.scenario to show how batch and ignore failure modes can be used to generate a complete scenario report with successful, failed and pending steps.

--- trunk/core/jbehave-ant/src/main/java/org/jbehave/ant/ScenarioRunnerTask.java
+++ trunk/core/jbehave-ant/src/main/java/org/jbehave/ant/ScenarioRunnerTask.java
@@ -3,6 +3,9 @@ package org.jbehave.ant;
 import static org.apache.tools.ant.Project.MSG_INFO;
 import static org.apache.tools.ant.Project.MSG_WARN;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.tools.ant.BuildException;
 import org.jbehave.scenario.RunnableScenario;
 
@@ -13,25 +16,60 @@ import org.jbehave.scenario.RunnableScenario;
  */
 public class ScenarioRunnerTask extends AbstractScenarioTask {
 
+    /**
+     * The boolean flag to run in batch mode
+     */
+    private boolean batch;
+
     public void execute() throws BuildException {
         if (skipScenarios()) {
             log("Skipped running scenarios", MSG_INFO);
             return;
         }
+
+        Map<String, Throwable> failedScenarios = new HashMap<String, Throwable>();
         for (RunnableScenario scenario : scenarios()) {
             String scenarioName = scenario.getClass().getName();
             try {
                 log("Running scenario " + scenarioName);
                 scenario.runScenario();
             } catch (Throwable e) {
-                String message = "Failed to run scenario " + scenarioName;
-                if (ignoreFailure()) {
-                    log(message, e, MSG_WARN);
+                String message = "Failure in running scenario " + scenarioName;
+                if (batch) {
+                    // collect and postpone decision to throw exception
+                    failedScenarios.put(scenarioName, e);
                 } else {
-                    throw new BuildException(message, e);
+                    if (ignoreFailure()) {
+                        log(message, e, MSG_WARN);
+                    } else {
+                        throw new BuildException(message, e);
+                    }
                 }
+            }
+        }
+        if (batch && failedScenarios.size() > 0) {
+            String message = "Failure in runing scenarios: " + format(failedScenarios);
+            if (ignoreFailure()) {
+                log(message, MSG_WARN);
+            } else {
+                throw new BuildException(message);
             }
         }
     }
 
+    private String format(Map<String, Throwable> failedScenarios) {
+        StringBuffer sb = new StringBuffer();
+        for (String scenarioName : failedScenarios.keySet()) {
+            Throwable cause = failedScenarios.get(scenarioName);
+            sb.append("\n");
+            sb.append(scenarioName);
+            sb.append(": ");
+            sb.append(cause.getMessage());
+        }
+        return sb.toString();
+    }
+
+    public void setBatch(boolean batch) {
+        this.batch = batch;
+    }
 }
