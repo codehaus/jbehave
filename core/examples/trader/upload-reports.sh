0c9f722cfdec95c74ac1939325396de51a0d6dd2
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-12-27 16:09:42 +0000

    Allow automatic override of existing examples reports.

--- core/examples/trader/upload-reports.sh
+++ core/examples/trader/upload-reports.sh
@@ -7,4 +7,4 @@ zip -r jbehave-trader-reports.zip jbehave-trader-reports
 
 scp jbehave-trader-reports.zip jbehave.org:
 
-ssh jbehave.org "rm -r jbehave-trader-reports; unzip jbehave-trader-reports.zip; mv jbehave-trader-reports /var/www/jbehave.org/reference/examples/"
+ssh jbehave.org "rm -r jbehave-trader-reports; unzip -uo jbehave-trader-reports.zip; rm -r /var/www/jbehave.org/reference/examples/jbehave-trader-reports; mv -f jbehave-trader-reports /var/www/jbehave.org/reference/examples"
