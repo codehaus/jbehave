afaa78b68da638fa57de08523cd0727b69f35484
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-12-27 15:51:16 +0000

    Upload reports to examples path to prevent them being removed when uploading docs.

--- core/examples/trader/upload-reports.sh
+++ core/examples/trader/upload-reports.sh
@@ -7,4 +7,4 @@ zip -r jbehave-trader-reports.zip jbehave-trader-reports
 
 scp jbehave-trader-reports.zip jbehave.org:
 
-ssh jbehave.org "rm -r jbehave-trader-reports; unzip jbehave-trader-reports.zip; mv jbehave-trader-reports /var/www/jbehave.org/reference/latest/"
+ssh jbehave.org "rm -r jbehave-trader-reports; unzip jbehave-trader-reports.zip; mv jbehave-trader-reports /var/www/jbehave.org/reference/examples/"
