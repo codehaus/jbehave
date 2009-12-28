0346716addddc67576dfa67c9ccc19314b95507a
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-12-28 15:17:30 +0000

    Added toString() to ErrorStrategy implementations.

--- trunk/core/jbehave-core/src/main/java/org/jbehave/scenario/errors/PendingErrorStrategy.java
+++ trunk/core/jbehave-core/src/main/java/org/jbehave/scenario/errors/PendingErrorStrategy.java
@@ -4,26 +4,33 @@ package org.jbehave.scenario.errors;
  * PendingErrorStrategy allows to define how pending error are handled. Two
  * standard strategies are provided:
  * <ul>
- * <li>{@link PASSING}: passes scenarios upon pending errors</li>
- * <li>{@link FAILING}: fails scenario upon pending errors</li>
+ * <li>{@link PendingErrorStrategy#PASSING}: passes scenarios upon pending errors</li>
+ * <li>{@link PendingErrorStrategy#FAILING}: fails scenario upon pending errors</li>
  * </ul>
  */
 public interface PendingErrorStrategy extends ErrorStrategy {
 
-	/**
-	 * Strategy that passes scenarios upon pending errors
-	 */
-	PendingErrorStrategy PASSING = new PendingErrorStrategy() {
-		public void handleError(Throwable throwable) {
-		}
-	};
+    /**
+     * Strategy that passes scenarios upon pending errors
+     */
+    PendingErrorStrategy PASSING = new PendingErrorStrategy() {
+        public void handleError(Throwable throwable) {
+        }
 
-	/**
-	 * Strategy that fails scenario upon pending errors
-	 */
-	PendingErrorStrategy FAILING = new PendingErrorStrategy() {
-		public void handleError(Throwable throwable) throws Throwable {
-			throw throwable;
-		}
-	};
+        public String toString() {
+            return "PASSING";
+        };
+    };
+
+    /**
+     * Strategy that fails scenario upon pending errors
+     */
+    PendingErrorStrategy FAILING = new PendingErrorStrategy() {
+        public void handleError(Throwable throwable) throws Throwable {
+            throw throwable;
+        }
+        public String toString() {
+            return "FAILING";
+        };
+    };
 }
