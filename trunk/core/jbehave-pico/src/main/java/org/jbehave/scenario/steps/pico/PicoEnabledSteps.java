45d95b4a1244783c09f41ea028904c9172092f4d
paul <paul@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-01-04 21:27:53 +0000

    better implementation for container passing

--- trunk/core/jbehave-pico/src/main/java/org/jbehave/scenario/steps/pico/PicoEnabledSteps.java
+++ trunk/core/jbehave-pico/src/main/java/org/jbehave/scenario/steps/pico/PicoEnabledSteps.java
@@ -8,37 +8,18 @@ import java.lang.reflect.Method;
 
 public class PicoEnabledSteps extends Steps {
 
-    private PicoContainer parent;
-
-    public PicoEnabledSteps() {
-        super();
-    }
-
-    public PicoEnabledSteps(KeyWords keywords) {
-        super(keywords);
-    }
-
-    public PicoEnabledSteps(ParameterConverters converters) {
-        super(converters);
-    }
-
-    public PicoEnabledSteps(StepsConfiguration configuration) {
+    public PicoEnabledSteps(PicoEnabledStepsConfiguration configuration) {
         super(configuration);
     }
 
-    public PicoEnabledSteps withParentContainer(PicoContainer parent) {
-        this.parent = parent;
-        return this;
-    }
-
     protected CandidateStep createCandidateStep(Method method, StepType stepType, String stepPatternAsString, StepsConfiguration configuration) {
-        if (parent == null) {
-            throw new NullPointerException("Parent container should be provided via PicoEnabledSteps#withParentContainer(PicoContainer)");
+        if (!(configuration instanceof PicoEnabledStepsConfiguration)) {
+            throw new PicoEnabledException("StepsConfiguration parameter should be instanceof PicoEnabledStepsConfiguration");
         }
         return new PicoEnabledCandidateStep(stepPatternAsString, stepType, method,
                 this, configuration.getPatternBuilder(),
                 configuration.getParameterConverters(),
-                configuration.getStartingWordsByType(), parent);
+                configuration.getStartingWordsByType(), ((PicoEnabledStepsConfiguration) configuration).getParentContainer());
     }
 
 
