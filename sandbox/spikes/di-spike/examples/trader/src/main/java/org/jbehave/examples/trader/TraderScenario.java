d68aef2c4f588ecea1e004126f0f2086e7388af9
paul <paul@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-01-08 18:35:11 +0000

    move di stuff to spike

---
+++ sandbox/spikes/di-spike/examples/trader/src/main/java/org/jbehave/examples/trader/TraderScenario.java
@@ -0,0 +1,69 @@
+package org.jbehave.examples.trader;
+
+import static java.util.Arrays.asList;
+import static org.jbehave.scenario.reporters.ScenarioReporterBuilder.Format.CONSOLE;
+import static org.jbehave.scenario.reporters.ScenarioReporterBuilder.Format.HTML;
+import static org.jbehave.scenario.reporters.ScenarioReporterBuilder.Format.TXT;
+import static org.jbehave.scenario.reporters.ScenarioReporterBuilder.Format.XML;
+
+import org.jbehave.examples.trader.converters.TraderConverter;
+import org.jbehave.examples.trader.model.Stock;
+import org.jbehave.examples.trader.model.Trader;
+import org.jbehave.examples.trader.persistence.TraderPersister;
+import org.jbehave.scenario.JUnitScenario;
+import org.jbehave.scenario.PropertyBasedConfiguration;
+import org.jbehave.scenario.RunnableScenario;
+import org.jbehave.scenario.parser.ClasspathScenarioDefiner;
+import org.jbehave.scenario.parser.PatternScenarioParser;
+import org.jbehave.scenario.parser.PrefixCapturingPatternBuilder;
+import org.jbehave.scenario.parser.ScenarioDefiner;
+import org.jbehave.scenario.parser.ScenarioNameResolver;
+import org.jbehave.scenario.parser.UnderscoredCamelCaseResolver;
+import org.jbehave.scenario.reporters.FilePrintStreamFactory;
+import org.jbehave.scenario.reporters.ScenarioReporter;
+import org.jbehave.scenario.reporters.ScenarioReporterBuilder;
+import org.jbehave.scenario.steps.ParameterConverters;
+import org.jbehave.scenario.steps.SilentStepMonitor;
+import org.jbehave.scenario.steps.StepMonitor;
+import org.jbehave.scenario.steps.StepsConfiguration;
+import org.jbehave.scenario.steps.StepsFactory;
+
+public class TraderScenario extends JUnitScenario {
+
+    private static ScenarioNameResolver converter = new UnderscoredCamelCaseResolver(".scenario");
+
+    public TraderScenario(final Class<? extends RunnableScenario> scenarioClass) {
+        super(new PropertyBasedConfiguration() {
+            @Override
+            public ScenarioDefiner forDefiningScenarios() {
+                return new ClasspathScenarioDefiner(converter, new PatternScenarioParser(keywords()));
+            }
+
+            @Override
+            public ScenarioReporter forReportingScenarios() {
+                return new ScenarioReporterBuilder(new FilePrintStreamFactory(scenarioClass, converter))
+                            .with(CONSOLE)
+                            .with(TXT)
+                            .with(HTML)
+                            .with(XML)
+                            .build();
+            }
+
+        });
+
+        StepsConfiguration configuration = new StepsConfiguration();
+        StepMonitor monitor = new SilentStepMonitor();
+		configuration.useParameterConverters(new ParameterConverters(
+        		monitor, new TraderConverter(mockTradePersister())));  // define converter for custom type Trader
+        configuration.usePatternBuilder(new PrefixCapturingPatternBuilder("%")); // use '%' instead of '$' to identify parameters
+        configuration.useMonitor(monitor);
+        
+        addSteps(new StepsFactory(configuration).createCandidateSteps(new TraderSteps()));
+    }
+
+    private TraderPersister mockTradePersister() {
+        return new TraderPersister(new Trader("Mauro", asList(new Stock("STK1", 10.d))));
+    }
+
+
+}
