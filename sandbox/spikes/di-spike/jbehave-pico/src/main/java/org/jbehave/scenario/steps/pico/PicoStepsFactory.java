22fe2014c895a672f239e41aff1c71dbeb9eedfb
paul <paul@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-01-10 02:26:45 +0000

    less code

--- sandbox/spikes/di-spike/jbehave-pico/src/main/java/org/jbehave/scenario/steps/pico/PicoStepsFactory.java
+++ sandbox/spikes/di-spike/jbehave-pico/src/main/java/org/jbehave/scenario/steps/pico/PicoStepsFactory.java
@@ -1,8 +1,5 @@
 package org.jbehave.scenario.steps.pico;
 
-import org.jbehave.scenario.annotations.Given;
-import org.jbehave.scenario.annotations.Then;
-import org.jbehave.scenario.annotations.When;
 import org.jbehave.scenario.steps.CandidateSteps;
 import org.jbehave.scenario.steps.Steps;
 import org.jbehave.scenario.steps.StepsConfiguration;
@@ -12,7 +9,6 @@ import org.picocontainer.MutablePicoContainer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class PicoStepsFactory {
@@ -25,13 +21,9 @@ public class PicoStepsFactory {
     }
 
     public CandidateSteps[] createCandidateSteps() {
-
-        List<CandidateSteps> steps = new ArrayList<CandidateSteps>();
-
-        Collection<ComponentAdapter<?>> adapters = parent.getComponentAdapters();
-        for (ComponentAdapter<?> componentAdapter : adapters) {
-            Class<?> impl = componentAdapter.getComponentImplementation();
-            Method[] methods = impl.getMethods();
+        List<Steps> steps = new ArrayList<Steps>();
+        for (ComponentAdapter<?> componentAdapter : parent.getComponentAdapters()) {
+            Method[] methods = componentAdapter.getComponentImplementation().getMethods();
             if (isStep(methods)) {
                 steps.add(new Steps(configuration, parent.getComponent(componentAdapter.getComponentKey())));
             }
@@ -41,9 +33,8 @@ public class PicoStepsFactory {
 
     private boolean isStep(Method[] methods) {
         for (Method method : methods) {
-            Annotation[] anns = method.getAnnotations();
-            for (Annotation ann : anns) {
-                if (ann.annotationType() == Given.class || ann.annotationType() == When.class || ann.annotationType() == Then.class) {
+            for (Annotation ann : method.getAnnotations()) {
+                if (ann.annotationType().getName().startsWith("org.jbehave.scenario.annotations")) {
                     return true;
                 }
             }
