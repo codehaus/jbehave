436dbb72cab09cb9f0b62ffd6941dd3de943b58d
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-05-03 22:59:50 +0000

    JBEHAVE-281:  Ensured the previous step for matching purposes is only a non And step, so it can be used to determine the step type compatibility with the current step.

--- trunk/core/examples/trader/src/main/java/org/jbehave/examples/trader/i18n/ItTraderSteps.java
+++ trunk/core/examples/trader/src/main/java/org/jbehave/examples/trader/i18n/ItTraderSteps.java
@@ -4,12 +4,14 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jbehave.Ensure.ensureThat;
 
 import java.util.Locale;
+import java.util.Map;
 
 import org.jbehave.examples.trader.model.Stock;
 import org.jbehave.scenario.annotations.Given;
 import org.jbehave.scenario.annotations.Named;
 import org.jbehave.scenario.annotations.Then;
 import org.jbehave.scenario.annotations.When;
+import org.jbehave.scenario.definition.ExamplesTable;
 import org.jbehave.scenario.definition.KeyWords;
 import org.jbehave.scenario.i18n.I18nKeyWords;
 import org.jbehave.scenario.i18n.StringEncoder;
@@ -19,11 +21,16 @@ import org.jbehave.scenario.steps.StepsConfiguration;
 public class ItTraderSteps extends Steps {
 
     private Stock stock;
+	private ExamplesTable table;
 
     public ItTraderSteps(ClassLoader classLoader) {
     	// Use Italian for keywords
         super(new StepsConfiguration(keywordsFor(new Locale("it"), classLoader)));
     }
+
+	protected static KeyWords keywordsFor(Locale locale, ClassLoader classLoader) {
+		return new I18nKeyWords(locale, new StringEncoder(), "org/jbehave/examples/trader/i18n/keywords", classLoader);
+	}
 
     @Given("ho un'azione con simbolo $symbol e una soglia di $threshold")
     public void aStock(@Named("symbol") String symbol, @Named("threshold") double threshold) {
@@ -40,8 +47,20 @@ public class ItTraderSteps extends Steps {
         ensureThat(stock.getStatus().name(), equalTo(status));
     }
 
-	protected static KeyWords keywordsFor(Locale locale, ClassLoader classLoader) {
-		return new I18nKeyWords(locale, new StringEncoder(), "org/jbehave/examples/trader/i18n/keywords", classLoader);
-	}
+    @Given("ho una tabella $table")
+    public void aTable(ExamplesTable table) {
+        this.table = table;
+    }
+
+    @Then("la tabella ha $rows righe")
+    public void hasRows(int rows){
+        ensureThat(table.getRowCount(), equalTo(rows));
+    }
+
+    @Then("alla riga $row e colonna $column troviamo: $value")
+    public void theRowValuesAre(int row, String column, String value){
+        Map<String,String> rowValues = table.getRow(row-1);      
+        ensureThat(rowValues.get(column), equalTo(value));
+    }
 
 }
