a73fd0763ddf2212a3af55ad993b59d249f0517d
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-05-20 11:16:51 +0000

    JBEHAVE-291:  Added method and steps instance information to match events in StepMonitor.

--- core/jbehave-core/src/main/java/org/jbehave/scenario/steps/StepMonitor.java
+++ core/jbehave-core/src/main/java/org/jbehave/scenario/steps/StepMonitor.java
@@ -1,5 +1,6 @@
 package org.jbehave.scenario.steps;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 
 /**
@@ -10,9 +11,9 @@ import java.lang.reflect.Type;
 public interface StepMonitor {
 
 	void stepMatchesType(String stepAsString, String previousAsString,
-			boolean matchesType, StepType stepType);
+			boolean matchesType, StepType stepType, Method method, Object stepsInstance);
 
-	void stepMatchesPattern(String step, boolean matches, String pattern);
+	void stepMatchesPattern(String step, boolean matches, String pattern, Method method, Object stepsInstance);
 
     void convertedValueOfType(String value, Type type, Object converted, Class<?> converterClass);
 
