1879b6b31583cd531189b53837eb65484e4fc421
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2010-06-07 22:09:34 +0000

    [maven-release-plugin]  copy for tag jbehave-2.5.9

---
+++ tags/jbehave-2.5.9/jbehave-core/src/main/java/org/jbehave/scenario/steps/StepMonitor.java
@@ -0,0 +1,34 @@
+package org.jbehave.scenario.steps;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+/**
+ * Interface to monitor step events
+ * 
+ * @author Mauro Talevi
+ */
+public interface StepMonitor {
+
+	void stepMatchesType(String stepAsString, String previousAsString,
+			boolean matchesType, StepType stepType, Method method, Object stepsInstance);
+
+	void stepMatchesPattern(String step, boolean matches, String pattern, Method method, Object stepsInstance);
+
+    void convertedValueOfType(String value, Type type, Object converted, Class<?> converterClass);
+
+    void performing(String step, boolean dryRun);
+
+	void usingAnnotatedNameForArg(String name, int position);
+
+	void usingParameterNameForArg(String name, int position);
+
+	void usingTableAnnotatedNameForArg(String name, int position);
+
+	void usingTableParameterNameForArg(String name, int position);
+
+	void usingNaturalOrderForArg(int position);
+
+	void foundArg(String arg, int position);
+
+}
